88f1e1bae200c9f6a5bc161c848e419b3f454d8f
Daniel Xie <Daniel Xie>
2017-05-22 10:39:51 -0500

    Rebalancing. Added slightly more money to the starting servers and gyms. Made many of the augmentations slightly cheaper

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 CONSTANTS = {
-    Version:                "0.13",
+    Version:                "0.14",
     
 	//Max level for any skill, assuming no multipliers. Determined by max numerical value in javascript for experience
     //and the skill level formula in Player.js. Note that all this means it that when experience hits MAX_INT, then
