89c8407cbe24eda5d91e0971552d0a22b12142ba
Daniel Xie <daniel.xie@xrtrading.local>
2017-07-04 16:18:42 -0500

    MOre work on Red Pill stuff

--- src/RedPill.js
+++ src/RedPill.js
@@ -1,4 +1,38 @@
-/* RedPill.js */
-function writeRedPillLine(line, i=0) {
-    
+/* RedPill.js 
+ *  Implements what happens when you have Red Pill augmentation and then hack the world daemon */
+ 
+//Returns promise
+function writeRedPillLine(line) {
+    return new Promise(function(resolve, reject) {
+        
+        var container = document.getElementById("red-pill-container");
+        var pElem = document.createElement("p");
+        container.appendChild(pElem);
+        
+        var promise = writeRedPillLetter(pElem, line, 0);
+        promise.then(function(res) {
+            resolve(res);
+        }, function(e) {
+            reject(e);
+        });
+    });
 }
+
+function writeRedPillLetter(pElem, line, i=0) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if (i >= line.length) {
+                resolve(true);
+            }
+            var textToShow = line.substring(0, i);
+            pElem.innerHTML = "> " + textToShow + "<span class='typed-cursor'> &#9608; </span>";
+            var promise = writeRedPillLetter(pElem, line, i+1);
+            promise.then(function(res) {
+                resolve(res);
+            }, function(e) {
+                reject(e);
+            });
+        }, 50);
+    });
+}
+
