1c0201534b695e95f7d446bdd3b5010f6d23b713
Olivier Gagnon <oli.gagnon4418@gmail.com>
2019-03-22 19:14:55 -0400

    reverse presentation order for BlackOps

--- src/Bladeburner.js
+++ src/Bladeburner.js
@@ -2166,9 +2166,7 @@ Bladeburner.prototype.createBlackOpsContent = function() {
         return (a.reqdRank - b.reqdRank);
     });
 
-    console.log(blackops);
     for (var i = blackops.length-1; i >= 0 ; --i) {
-      console.log(i)
       if (this.blackops[[blackops[i].name]] == null && i !== 0 && this.blackops[[blackops[i-1].name]] == null) {continue;} // If this one nor the next are completed then this isn't unlocked yet.
         DomElems.blackops[blackops[i].name] = createElement("div", {
             class:"bladeburner-action", name:blackops[i].name
