96db360a3670c0735a998b18e5ddcb668cb1ff7f
Olivier Gagnon <oli.gagnon4418@gmail.com>
2019-03-23 00:22:40 -0400

    added sleeve aug api

--- src/ScriptEditor/AceNetscriptMode.js
+++ src/ScriptEditor/AceNetscriptMode.js
@@ -128,9 +128,10 @@ let NetscriptFunctions =
     "getNumTriesRemaining|"                                                    +
 
     // Sleeve API
-    "sleeve|getNumSleeves|setToShockRecovery|setToSynchronize|setToCommitCrime|"              +
-    "setToUniversityCourse|travel|setToCompanyWork|setToFactionWork|setToGymWorkout|"  +
-    "getSleeveStats|getTask|getInformation";
+    "sleeve|getNumSleeves|setToShockRecovery|setToSynchronize|"                +
+    "setToCommitCrime|setToUniversityCourse|travel|setToCompanyWork|"          +
+    "setToFactionWork|setToGymWorkout|getSleeveStats|getTask|getInformation|"  +
+    "getSleeveAugmentations|getSleevePurchasableAugs|purchaseSleeveAug";
 
 var NetscriptHighlightRules = function(options) {
     var keywordMapper = this.createKeywordMapper({
