193d799b79c3b78ede6de0129b547d46743f5471
Helldragger <christopher1223334444@live.fr>
2019-07-25 23:15:23 +0200

    ls is now returning a path per line as expected.

--- src/Server/lib/ls.ts
+++ src/Server/lib/ls.ts
@@ -14,14 +14,13 @@ import * as path from 'path';
  * @param {number} [nodeLimit=50] The limit of files to parse, in order to avoid problems with large repositories. Set to -1 to disable.
  * @returns {string} The String representation of the file tree in a record manner.
  */
-export function ls(server: BaseServer, term:any, args:string[], targetDir:string|undefined=undefined, depth: number= 2, nodeLimit= 50): string {
+export function ls(server: BaseServer, term:any, args:string[], targetDir:string|undefined=undefined, depth: number= 2): string {
     const TOO_MANY_ARGUMENTS_ERROR: string = "Too many arguments";
     const INVALID_PATH_ERROR: string = "Invalid path";
     const HELP_MESSAGE: string = "Incorrect usage of ls command. Usage: ls <--depth -d> number <--limit -l> number <targetDir>";
     let error: string;
     let cwd:string = term.currDir;
     depth = 2;
-    nodeLimit = 50;
     console.log(`@ ${server.hostname} > ${cwd} ] ls called with the following arguments : ${JSON.stringify(args)}`);
     while (args.length > 0) {
         console.log(`args stack left: ${JSON.stringify(args)}`);
@@ -30,12 +29,6 @@ export function ls(server: BaseServer, term:any, args:string[], targetDir:string
             case "-h":
             case "--help": 
                 throw HELP_MESSAGE;
-            case "-l":
-            case "--limit":
-                console.log(`limit flag detected, args stack left: ${JSON.stringify(args)}`);
-                if (args.length > 0) nodeLimit = parseInt(args.shift() as string);
-                else throw HELP_MESSAGE; 
-                break;
             case "-d": 
             case "--depth":
                 console.log(`depth flag detected, args stack left: ${JSON.stringify(args)}`);
@@ -64,12 +57,9 @@ export function ls(server: BaseServer, term:any, args:string[], targetDir:string
         processed.add(node.path+node.name);
         const dirContent = server.readdir(node.path+node.name, true);
         if (!dirContent) { return `An error occured when parsing the content of the ${node.name} directory.`; }
-        for (let c = 0; c < Math.min(dirContent.length, nodeLimit); c++) {
-            nodeLimit--;
+        for (let c = 0; c < dirContent.length; c++) {
             const fileInfo = dirContent[c];
             if (processed.has(fileInfo.name)) { break; } // avoid circular dependencies due to symlinks
-
-
             const filetype = detectFileType(fileInfo);
             if (filetype == FileType.FILE || filetype == FileType.DIRECTORY) {
                 const childrenNode = new TreeNode(fileInfo.name, detectFileType(fileInfo));
@@ -78,11 +68,6 @@ export function ls(server: BaseServer, term:any, args:string[], targetDir:string
                     toBeProcessed.push(childrenNode);
                 }
             }
-        }
-        // node limit in case of enormous repositories.
-        if (nodeLimit == 0) {
-            console.log("node limit reached");
-            return "ERROR: Too much nodes to discover.";
         }
     }
     return rootNode.toString(true);
@@ -105,7 +90,7 @@ class TreeNode {
     }
     toString(isLast = false): string {
         const localResults: string[] = [];
-        localResults.push([SCOPE.repeat(Math.max(0, this.depth - 1)), ((this.depth > 0) ? ((isLast) ? LAST : BRANCH) : ""), this.name, (this.fileType == FileType.DIRECTORY && this.name != "/") ? "/" : ""].join(""));
+        localResults.push([this.path, this.name, (this.fileType == FileType.DIRECTORY && this.name != "/") ? "/" : ""].join(""));
         if (this.childrens.length > 0) {
             for (let i = 0; i < this.childrens.length; i++) {
                 localResults.push(this.childrens[i].toString(i == (this.childrens.length - 1)));
