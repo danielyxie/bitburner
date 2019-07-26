bf45282a513e60bbf21d9a43e9abc3049e4bc58a
Helldragger <christopher1223334444@live.fr>
2019-07-26 11:16:46 +0200

    fixed the last known bugs of rm

--- src/Server/lib/rm.ts
+++ src/Server/lib/rm.ts
@@ -14,68 +14,86 @@ import { detectFileType } from "./FileType";
  * @param {boolean} [force=false] If 'true' this option will destroy the target directly, instead of moving it into the trash bin (/~trash/).
  * @returns
  */
-export function rm(server: BaseServer, term: any, args: string[], targetPath: string | undefined= undefined, recursive: boolean= false, force: boolean= false) {
+export function rm(server: BaseServer, term: any, out:Function, err:Function, args: string[], options:any={targetPath:undefined, recursive:false, force:false}) {
     const TOO_MANY_ARGUMENTS_ERROR: string = "Too many arguments";
     const INVALID_PATH_ERROR: string = "Invalid path";
     const HELP_MESSAGE: string = "Incorrect usage of rm command. Usage: rm <-rf> [target]";
     let error: string;
     const cwd: string = term.currDir;
+    let paths:string[] = [];
     while (args.length > 0) {
         const arg = args.shift();
         switch (arg) {
             case "-h":
                 throw HELP_MESSAGE;
             case "-r":
-                recursive = true;
+                    options.recursive = true;
                 break;
             case "-f":
-                force = true;
+                    options.force = true;
                 break;
             default:
-                if (!targetPath) { targetPath = arg; } else { throw TOO_MANY_ARGUMENTS_ERROR + HELP_MESSAGE; }
+                paths.push(arg as string);
                 break;
         }
     }
-    if (!targetPath) { throw HELP_MESSAGE; } else {
-        targetPath = evaluateDirectoryPath(server, targetPath, cwd);
-        if (!targetPath) { throw INVALID_PATH_ERROR; } else {
-            remove(server, targetPath, { force, recursive });
-            return 1;
+    if (paths.length==0) { throw HELP_MESSAGE; } else {
+        for(let targetPath of paths){
+            let evaluatedPath = term.getFilepath(targetPath);
+            if (!evaluatedPath) { throw INVALID_PATH_ERROR; } else {
+                remove(server, term, out, err, evaluatedPath, options);
+                return 1;
+            }
         }
     }
 }
 
-function removeFile(server: BaseServer, targetPath: string, options: any) {
-    server.removeFile(targetPath, options);
+function removeFile(server: BaseServer, term: any, out:Function, err:Function, targetPath: string, options: any) {
+    if (!server.exists(targetPath)) {
+        throw  `${targetPath} doesn't exists`;
+    } else if (server.isRunning(targetPath)) {
+        throw "Cannot delete a script that is currently running!";
+    } else {
+        if (options.force) {
+            out(`Removing ${targetPath}`); server.fs.unlinkSync(targetPath);
+            if(options.verbose) out(`'${targetPath}' -> '/dev/null'`)
+        }
+        else{
+            server.moveFile(targetPath, "/~trash/" +  targetPath, {recursive:true});
+            if(options.verbose) out(`'${targetPath}' -> '${"/~trash/"+targetPath}'`)
+        };
+    }
 }
 
-function remove(server: BaseServer,  targetPath: string, options: any) {
+function remove(server: BaseServer, term: any, out:Function, err:Function,  targetPath: string, options: any) {
 
     try {
         if (!options.recursive) { // if the recursive option is not activated, the directory is not scanned
-            return server.removeDir(targetPath, options);
+            return server.fs.rmdirSync(targetPath);
         }
     } catch (e1) {
         try {
-            return removeFile(server, targetPath, options);
+            // this is called if the path is a file.
+            return removeFile(server, term, out, err, targetPath, options);
         } catch (e2) {
+            //this happens if the path is not a file, nor a directory.
             throw e2;
         }
     }
-
+    if (server.isDir(targetPath)) targetPath += (targetPath.endsWith("/"))? "": "/";
     // we scan the wanted directory before anything else.
-    const dirContent = server.readdir(targetPath, {withFileTypes: true}) ;
+    const dirContent = server.readdir(targetPath, {withFileTypes: true});
+
     for (let i = 0; i < dirContent.length; i++) {
         const object = dirContent[i];
         if (object.isDirectory()) {
-            remove(server,  targetPath + object.name, options);
+            remove(server, term, out, err,  targetPath + object.name, options);
         } else if (object.isFile()) {
-            removeFile(server,  targetPath + object.name, options);
-             } else {
+            removeFile(server, term, out, err,  targetPath + object.name, options);
+        } else {
             throw new Error(`ERRTYPE: Type unimplemented, cannot remove ${JSON.stringify(targetPath + object.name)} : ${detectFileType(object)}`);
-             }
+        }
     }
-    console.log(`Removing directory ${JSON.stringify(targetPath)}`);
-    server.removeDir(targetPath, options);
-
+    server.fs.rmdirSync(targetPath);
+    if(options.verbose) out(`'${targetPath}' -> '/dev/null'`)
 }
