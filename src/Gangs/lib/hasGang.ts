d710d34c6372e3e367733691a974c4155cd1aae3
Helldragger <chritopher1223334444@gmail.com>
2019-12-17 22:17:52 +0100

    add hasGang check executable as gang function

---
+++ src/Gangs/lib/hasGang.ts
@@ -0,0 +1,15 @@
+import { BaseServer } from "../../Server/BaseServer";
+import { Gang } from "../../Gangs";
+
+export function hasGang(server: BaseServer, term: any, out:Function, err:Function, args: string[], options:any={}) {
+    out(term.getPlayer().gang instanceof Gang);
+}
+
+import { registerExecutable, ManualEntry } from "../../Server/lib/sys";
+
+const MANUAL = new ManualEntry(
+`hasGang - Returns whether you currently have a Gang or not.`,
+`hasGang`,
+`Returns whether you currently have a Gang or not.`);
+
+registerExecutable("hasGang", hasGang, MANUAL, true, "gang");
