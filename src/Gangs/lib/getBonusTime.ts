d08bb93904e79165abeddfe4cd50a2ea9f0b6173
Helldragger <chritopher1223334444@gmail.com>
2019-12-18 13:22:50 +0100

    add getBonusTime

--- src/Gangs/lib/getBonusTime.ts
+++ src/Gangs/lib/getBonusTime.ts
@@ -1,15 +1,15 @@
 import { BaseServer } from "../../Server/BaseServer";
+import { Gang} from "../../Gang";
 
 export function getBonusTime(server: BaseServer, term: any, out:Function, err:Function, args: string[], options:any={type:false, list:false}) {
 
-    try {
-        let result = Math.round(term.getPlayer().gang.storedCycles / 5);
-        out(result);
-        return true;
-    } catch(e) {
-        err( e );
+    if(! (term.getPlayer().gang instanceof Gang)){
+        err(`You have no gang`)
         return false;
     }
+    let result = Math.round(term.getPlayer().gang.storedCycles / 5);
+    out(result);
+    return true;
 }
 
 import { registerExecutable, ManualEntry } from "../../Server/lib/sys";
