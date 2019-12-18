b1d0105ba9fbff4153c9435a2cb0c38c62d98201
Helldragger <chritopher1223334444@gmail.com>
2019-12-18 20:15:11 +0100

    add purchaseEquipment executable

---
+++ src/Gangs/lib/purchaseEquipment.ts
@@ -0,0 +1,114 @@
+import { BaseServer } from "../../Server/BaseServer";
+import { hasGang } from "./hasGang";
+import {GangMemberUpgrades, GangMemberUpgrade, UIElems} from "../../Gang";
+
+import { Page, routing } from "../../ui/navigationTracking";
+
+export function purchaseEquipment(server: BaseServer, term: any, out:Function, err:Function, args: string[], options:any={type:false, list:false}) {
+
+    const HELP_MESSAGE: string = "Usage: purchaseEquipment [-e] EQUIPNAME [-m] MEMBERNAME";
+    let memberName:string|undefined;
+    let equipName:string|undefined;
+    while (args.length > 0) {
+        const arg = args.shift() as string;
+        switch (arg) {
+            case "-h":
+            case "--help":
+                out(HELP_MESSAGE);
+                return false;
+            case "-e":
+            case "--equip":
+                if(args.length < 1){
+                    err(`No equipment specified after -e`)
+                    return false;
+                }
+                equipName = args.shift() as string;
+                break;
+            case "-m":
+            case "--member":
+                if(args.length < 1){
+                    err(`No member specified after -m`)
+                    return false;
+                }
+                memberName = args.shift() as string;
+                break;
+            default:
+                if(memberName === undefined){
+                    memberName = arg as string;
+                }else if(equipName === undefined){
+                    equipName = arg as string;
+                }else{
+                    err(`Too many arguments: ${arg}`);
+                    return false;
+                }
+                break;
+        }
+    }
+
+    let player = term.getPlayer();
+    let hasGangAlready:boolean = false;
+    let hasGangOut = (value:boolean)=>{hasGangAlready=value;};
+    hasGang(server, term, hasGangOut, err, [])
+    if(!hasGangAlready){
+        err(`You dont have a gang`);
+        return false;
+    }
+    let gang = player.gang;
+    try {
+        const member = term.getPlayer().gang.getMember(memberName);
+        if (member === undefined){
+            err(`No gang member could be found with name ${memberName}`);
+            return false;
+        }
+
+        if (typeof equipName === 'string') {
+            var upg = GangMemberUpgrades[equipName];
+        }
+
+        if (!(upg instanceof GangMemberUpgrade)) {
+            err(`No equipment could be found with name ${equipName}`);
+            return false;
+        }
+
+        if (member.augmentations.includes(upg.name) || member.upgrades.includes(upg.name)) {
+            err(`${memberName} already has ${equipName}`);
+            return false;
+        }
+
+        if (player.money.lt(upg.getCost(gang))) {
+            err(`You do not have enough money to afford this.`)
+            return false;
+        }
+
+        player.loseMoney(upg.getCost(gang));
+
+        if (upg.type === "g") {
+            member.augmentations.push(upg.name);
+        } else {
+            member.upgrades.push(upg.name);
+        }
+
+        upg.apply(member);
+
+        if (routing.isOn(Page.Gang) && UIElems.gangMemberUpgradeBoxOpened) {
+            var initFilterValue = UIElems.gangMemberUpgradeBoxFilter.value.toString();
+            gang.createGangMemberUpgradeBox(player, initFilterValue);
+        }
+
+        out(`Purchased ${equipName} for Gang member ${memberName}`);
+
+        return true;
+    } catch(e) {
+        err(e);
+    }
+}
+
+import { registerExecutable, ManualEntry } from "../../Server/lib/sys";
+import { mem } from "src/Server/lib/mem";
+
+const MANUAL = new ManualEntry(
+`purchaseEquipment - Purchase a specific equipment for a specific gang member.`,
+`purchaseEquipment [-e] EQUIPNAME [-m] MEMBERNAME`,
+`Purchase a specific equipment for a specific gang member.`);
+
+registerExecutable("purchaseEquipment", purchaseEquipment, MANUAL, true, "gang");
