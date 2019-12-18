26ffc5c06a9ebff302d5de9a1c8a95c6f11bb49e
Helldragger <chritopher1223334444@gmail.com>
2019-12-18 22:10:45 +0100

    add getEquipmentCost executable

---
+++ src/Gangs/lib/getEquipmentCost.ts
@@ -0,0 +1,45 @@
+import { BaseServer } from "../../Server/BaseServer";
+import {GangMemberUpgrades} from "../../Gang";
+import { hasGang } from "./hasGang";
+
+export function getEquipmentCost(server: BaseServer, term: any, out:Function, err:Function, args: string[], options:any={type:false, list:false}) {
+    let hasGangAlready:boolean = false;
+    let hasGangOut = (value:boolean)=>{hasGangAlready=value;};
+    hasGang(server, term, hasGangOut, err, [])
+    if(!hasGangAlready){
+        err(`You dont have a gang`);
+        return false;
+    }
+    if (args.length == 0){
+        err(`You must provide at least one equipment`)
+        return false;
+    }
+    let multiple = args.length > 1;
+    let discount = term.getPlayer().gang.getDiscount()
+
+    for(let upgName of args){
+        const upg = GangMemberUpgrades[upgName];
+        if (upg === undefined){
+            if (!multiple) {
+                err(`Unknown equipment ${upgName}`);
+                return false;
+            }
+            out(`Unknown equipment ${upgName}`);
+            continue;
+        }
+
+        let actualCost = upg.cost / discount
+        if (!multiple) out(`${actualCost}`)
+        else out(`${upgName}\t${actualCost}`);
+    }
+    return true;
+}
+import { registerExecutable, ManualEntry } from "../../Server/lib/sys";
+
+const MANUAL = new ManualEntry(
+`getEquipmentCost - Outputs the cost of specified equipments.`,
+`getEquipmentCost EQUIPNAME
+getEquipmentCost EQUIPNAME ...`,
+`Outputs the cost of specified equipments.`);
+
+registerExecutable("getEquipmentCost", getEquipmentCost, MANUAL, true, "gang");
