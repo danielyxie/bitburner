8ee4802f5f6ab7efa9658ffd835374f376b89e85
Helldragger <chritopher1223334444@gmail.com>
2019-12-19 07:01:25 +0100

    add getMemberInformation.ts executable
    Signed-off-by: Helldragger <chritopher1223334444@gmail.com>

---
+++ src/Gangs/lib/getMemberInformation.ts
@@ -0,0 +1,54 @@
+import { BaseServer } from "../../Server/BaseServer";
+import { ManualEntry, registerExecutable } from "../../Server/lib/sys";
+import { throwIfNoGang } from "./throwIfNoGang";
+
+export function getMemberInformation(server: BaseServer, term: any, out: Function, err: Function, args: string[], options: any = {}) {
+    throwIfNoGang(server, term, err);
+    const gang = term.getPlayer().gang;
+    if (args.length !== 1) {
+        err(`You must provide a name and only one`);
+        return false;
+    }
+    const name: string = args.shift() as string;
+    if (name === "") {
+        err(`You must provide a non-empty name`);
+        return false;
+    }
+    if (!Object.keys(gang.members).includes(name)) {
+        err(`No member named ${name} found`);
+        return false;
+    }
+    const member = gang.members[name];
+
+    out({
+            agility: member.agi,
+            agilityEquipMult: member.agi_mult,
+            agilityAscensionMult: member.agi_asc_mult,
+            augmentations: member.augmentations.slice(),
+            charisma: member.cha,
+            charismaEquipMult: member.cha_mult,
+            charismaAscensionMult: member.cha_asc_mult,
+            defense: member.def,
+            defenseEquipMult: member.def_mult,
+            defenseAscensionMult: member.def_asc_mult,
+            dexterity: member.dex,
+            dexterityEquipMult: member.dex_mult,
+            dexterityAscensionMult: member.dex_asc_mult,
+            equipment: member.upgrades.slice(),
+            hacking: member.hack,
+            hackingEquipMult: member.hack_mult,
+            hackingAscensionMult: member.hack_asc_mult,
+            strength: member.str,
+            strengthEquipMult: member.str_mult,
+            strengthAscensionMult: member.str_asc_mult,
+            task: member.task,
+        });
+    return true;
+}
+
+const MANUAL = new ManualEntry(
+    `getMemberInformation - Returns a specific gang member information.`,
+    `getMemberInformation MEMBERNAME`,
+    `Returns a specific gang member information.`);
+
+registerExecutable("getMemberInformation", getMemberInformation, MANUAL, true, "gang");
