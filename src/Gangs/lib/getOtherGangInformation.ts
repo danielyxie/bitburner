8757d98adb53c990bfefb6b7946cad1c7bafdbd5
Helldragger <chritopher1223334444@gmail.com>
2019-12-19 07:20:14 +0100

    add getOtherGangInformation.ts executable
    Signed-off-by: Helldragger <chritopher1223334444@gmail.com>

---
+++ src/Gangs/lib/getOtherGangInformation.ts
@@ -0,0 +1,22 @@
+import { AllGangs } from "../../Gang";
+import { BaseServer } from "../../Server/BaseServer";
+import { ManualEntry, registerExecutable } from "../../Server/lib/sys";
+import { throwIfNoGang } from "./throwIfNoGang";
+
+export function getOtherGangInformation(server: BaseServer, term: any, out: Function, err: Function, args: string[], options: any = {}) {
+
+    throwIfNoGang(server, term, err);
+    const cpy: any = {};
+    for (const gang of AllGangs) {
+        cpy[gang] = {...AllGangs[gang]};
+    }
+    out(cpy);
+    return true;
+}
+
+const MANUAL = new ManualEntry(
+    `getOtherGangInformation - Outputs other information about all the gangs.`,
+    `getOtherGangInformation`,
+    `Outputs other information about all the gangs.`);
+
+registerExecutable("getOtherGangInformation", getOtherGangInformation, MANUAL, true, "gang");
