97ffee78055a292256c41e6c7021e2c413444728
F.N <Farbdose@users.noreply.github.com>
2020-05-05 11:27:49 +0200

    fix tslint

--- src/utils/PrototypeTamperingPrevention.ts
+++ src/utils/PrototypeTamperingPrevention.ts
@@ -1,65 +1,67 @@
 // Prevent modification of important prototypes
 
+// tslint:disable-next-line:interface-name
 interface Window {
-    [key:string]: any; // Add index signature
+    [key: string]: any; // Add index signature
 }
 
+// tslint:disable-next-line:interface-name
 interface Object {
-    [key:string]: any; // Add index signature
+    [key: string]: any; // Add index signature
 }
 
-function unconfigurable(parent: Object, keys: string[]) {
-    keys.forEach((key) => Object.defineProperty(parent, key, {
+function unconfigurable(parent: object, keys: string[]) {
+    keys.forEach((key: string) => Object.defineProperty(parent, key, {
         configurable: false,
+        value: (parent as any)[key],
         writable: false,
-        value: parent[key]
     }));
 }
 
 [
-    'String', 
-    ...Object.getOwnPropertyNames(window).filter((key) => key.startsWith('HTML'))
+    "String",
+    ...Object.getOwnPropertyNames(window)
+        .filter((key: string) => key.startsWith("HTML")),
 ]
-    .filter((key) => window[key])
-    .forEach((key) => {
+    .filter((key: string) => window[key])
+    .forEach((key: string) => {
         Object.freeze(window[key]);
         Object.freeze(window[key].prototype);
         unconfigurable(window, [key]);
     });
-    
+
 unconfigurable(Array.prototype, [
-    'pop', 
-    'push', 
-    'shift', 
-    'unshift', 
-    'map', 
-    'sort', 
-    'splice', 
-    'slice', 
-    'reduce',  
-    'reduceRight',  
-    'forEach' 
+    "pop",
+    "push",
+    "shift",
+    "unshift",
+    "map",
+    "sort",
+    "splice",
+    "slice",
+    "reduce",
+    "reduceRight",
+    "forEach",
 ]);
 
 unconfigurable(Function.prototype, [
-    'apply', 
-    'bind'
+    "apply",
+    "bind",
 ]);
 
 unconfigurable(Object, [
-    'values', 
-    'keys'
+    "values",
+    "keys",
 ]);
 
-unconfigurable(Array, ['prototype']);
-unconfigurable(Function, ['prototype']);
-unconfigurable(Object, ['prototype']);
+unconfigurable(Array, ["prototype"]);
+unconfigurable(Function, ["prototype"]);
+unconfigurable(Object, ["prototype"]);
 unconfigurable(window, [
-    'Array',
-    'Function',
-    'Object',
-    'eval', 
-    'setTimeout', 
-    'setInterval'
+    "Array",
+    "Function",
+    "Object",
+    "eval",
+    "setTimeout",
+    "setInterval",
 ]);
-   
