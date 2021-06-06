f6636f3bfad1570324526564091c73bbdf1d003e
Tesseract1234567890 <zihnalibora@gmail.com>
2021-06-06 12:22:02 -0400

    Removed text telling what augment does and removed console.log

--- src/Augmentation/AugmentationHelpers.jsx
+++ src/Augmentation/AugmentationHelpers.jsx
@@ -101,19 +101,8 @@ function initAugmentations() {
 
     const CircadianRhythm = new Augmentation({
         name:AugmentationNames.CircadianRhythm, moneyCost: 1e9, repCost:4.5e3,
-        info:"A prototype injection which modifies your circadian rhythm, leading to varied effects.<br><br>" +
-             "This augmentation currently modifies these values:<br>"
+        info:"A prototype injection which modifies your circadian rhythm, leading to varied effects."
     });
-
-    
-    for (let i = 0; i < randomBonuses.length; i++) {
-        console.log(`${randomBonuses[i]}`);
-        CircadianRhythm.mults[randomBonuses[i][0]] = randomBonuses[i][1];
-        CircadianRhythm.info += `${randomBonuses[i][0]} by ${Math.round(100 * randomBonuses[i][1].toFixed(2))}%<br>`
-    }
-
-
-    console.log(CircadianRhythm.info);
 
     CircadianRhythm.addToFactions(["Speakers for the Dead"]);
     if (augmentationExists(AugmentationNames.CircadianRhythm)) {
