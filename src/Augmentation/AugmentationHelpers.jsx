71b09362ef7fab35a2e4255d3de8e98265adb71b
Kwazygloo <85138054+Kwazygloo@users.noreply.github.com>
2021-06-18 10:58:05 -0400

    Update AugmentationHelpers.jsx

--- src/Augmentation/AugmentationHelpers.jsx
+++ src/Augmentation/AugmentationHelpers.jsx
@@ -54,13 +54,13 @@ function getRandomBonus() {
         {
             bonuses: {
                 strength_mult: 1.25,
-                strength_exp_mult: 1.5,
+                strength_exp_mult: 2,
                 defense_mult: 1.25,
-                defense_exp_mult: 1.5,
+                defense_exp_mult: 2,
                 dexterity_mult: 1.25,
-                dexterity_exp_mult: 1.5,
+                dexterity_exp_mult: 2,
                 agility_mult: 1.25,
-                agility_exp_mult: 1.5,
+                agility_exp_mult: 2,
             },
             description: "Increases all combat stats by 25%.<br>" +
                          "Increases all combat stat experience gain by 100%.",
