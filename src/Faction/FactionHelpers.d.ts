b31b3dc735db91d3ad4c02c5328512fee3cf1a73
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-08-15 16:20:10 -0400

    Factions list screen converted to React.

--- src/Faction/FactionHelpers.d.ts
+++ src/Faction/FactionHelpers.d.ts
@@ -6,3 +6,4 @@ export declare function hasAugmentationPrereqs(aug: Augmentation): boolean;
 export declare function purchaseAugmentationBoxCreate(aug: Augmentation, fac: Faction): void;
 export declare function purchaseAugmentation(aug: Augmentation, fac: Faction, sing?: boolean): void;
 export declare function displayFactionContent(factionName: string, initiallyOnAugmentationsPage: boolean=false);
+export declare function joinFaction(faction: Faction): void;
