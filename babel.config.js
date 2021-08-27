feaa74ed3486b15246bfdf550212b9a701f3d1dd
David Edmondson <threehams@gmail.com>
2021-08-26 17:02:02 -0700

    Only compile down imports during tests

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+const TEST = process.env.NODE_ENV === "test";
+
+module.exports = {
+    "presets": [
+        "@babel/preset-react",
+        TEST && "@babel/preset-env",
+        TEST && "@babel/preset-typescript",
+    ].filter(Boolean),
+}
