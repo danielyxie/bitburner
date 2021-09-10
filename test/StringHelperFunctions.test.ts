7e8e9e03fcc445ad31fe0412e51ac6e8b4be2dfc
hydroflame <79246165+hydroflame@users.noreply.github.com>
2021-09-10 00:13:03 -0400

    Revert "Jest cypress types"

--- test/StringHelperFunctions.test.ts
+++ test/StringHelperFunctions.test.ts
@@ -2,19 +2,19 @@ import { convertTimeMsToTimeElapsedString } from "../utils/StringHelperFunctions
 
 describe("StringHelperFunctions Tests", function () {
   it("transforms strings", () => {
-    expect(convertTimeMsToTimeElapsedString(1000)).toEqual("1 seconds");
-    expect(convertTimeMsToTimeElapsedString(5 * 60 * 1000 + 34 * 1000)).toEqual("5 minutes 34 seconds");
-    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000)).toEqual(
+    expect(convertTimeMsToTimeElapsedString(1000)).equal("1 seconds");
+    expect(convertTimeMsToTimeElapsedString(5 * 60 * 1000 + 34 * 1000)).equal("5 minutes 34 seconds");
+    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000)).equal(
       "2 days 5 minutes 34 seconds",
     );
-    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000, true)).toEqual(
+    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000, true)).equal(
       "2 days 5 minutes 34.000 seconds",
     );
-    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000 + 123, true)).toEqual(
+    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000 + 123, true)).equal(
       "2 days 5 minutes 34.123 seconds",
     );
-    expect(
-      convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000 + 123.888, true),
-    ).toEqual("2 days 5 minutes 34.123 seconds");
+    expect(convertTimeMsToTimeElapsedString(2 * 60 * 60 * 24 * 1000 + 5 * 60 * 1000 + 34 * 1000 + 123.888, true)).equal(
+      "2 days 5 minutes 34.123 seconds",
+    );
   });
 });
