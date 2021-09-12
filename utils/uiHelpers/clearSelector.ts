d515db084214a992c1156b7bf035ce97ec41d790
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-09-12 18:06:21 -0400

    remove some now unused files

--- utils/uiHelpers/clearSelector.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Clears all <option> elements from a <select>.
- * @param selector The <select> element
- */
-export function clearSelector(selector: HTMLSelectElement): void {
-  for (let i: number = selector.options.length - 1; i >= 0; i--) {
-    selector.remove(i);
-  }
-}
