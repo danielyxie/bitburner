da488e586b106ff81d291811e2b2d460f730abd9
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-09-24 17:16:14 -0400

    convert to ts

--- src/Faction/FactionHelpers.d.ts
+++
@@ -1,8 +0,0 @@
-import { Augmentation } from "../Augmentation/Augmentation";
-import { Faction } from "../Faction/Faction";
-
-export declare function getNextNeurofluxLevel(): number;
-export declare function hasAugmentationPrereqs(aug: Augmentation): boolean;
-export declare function purchaseAugmentation(aug: Augmentation, fac: Faction, sing?: boolean): void;
-export declare function joinFaction(faction: Faction): void;
-export declare function startHackingMission(faction: Faction): void;
--- src/Missions.d.ts
+++ src/Missions.d.ts
@@ -1,1 +1,6 @@
 export declare let inMission: boolean;
+export declare class HackingMission {
+  constructor(reputation: number, faction: Faction);
+  init(): void;
+}
+export declare function setInMission(inMission: boolean, mission: HackingMission): void;
