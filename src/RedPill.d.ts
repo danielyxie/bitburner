4f219a32143dbbbd7e77eba84ac2c9d1a7826726
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-09-24 16:37:42 -0400

    convert to ts

--- src/RedPill.d.ts
+++
@@ -1,3 +0,0 @@
-export declare let redPillFlag: boolean;
-export declare function enterBitNode(router: IRouter, flume: boolean, destroyedBitNode: number, newBitNode: number);
-export declare function setRedPillFlag(b: boolean): void;
