6bfec83d9136b40a9fb96d5c6ee2c02700357588
Snarling <84951833+Snarling@users.noreply.github.com>
2021-10-18 00:02:23 -0400

    Update GeneralActions.tsx

--- src/Bladeburner/GeneralActions.tsx
+++ src/Bladeburner/GeneralActions.tsx
@@ -4,15 +4,16 @@ import { IMap } from "../types";
 export const GeneralActions: IMap<Action> = {};
 
 let actionNames : Array<string> = [
-    "Training",
-    "Field Analysis",
-    "Recruitment",
-    "Diplomacy",
-    "Hyperbolic Regeneration Chamber",
-    "Incite Violence"
+  "Training",
+  "Field Analysis",
+  "Recruitment",
+  "Diplomacy",
+  "Hyperbolic Regeneration Chamber",
+  "Incite Violence"
 ];
+
 for (let actionName of actionNames){
-    GeneralActions[actionName] = new Action({
-        name: actionName,
-    });
+  GeneralActions[actionName] = new Action({
+    name: actionName,
+  });
 }
