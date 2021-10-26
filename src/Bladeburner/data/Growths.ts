6865fe646389fdc6bcd795cad8dafc7efbc5a5d1
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-10-26 19:55:34 -0400

    revert incite violence effect

--- src/Bladeburner/data/Growths.ts
+++ src/Bladeburner/data/Growths.ts
@@ -12,13 +12,13 @@ export const Growths: {
   ["Stealth Retirement Operation"]: () => number;
   ["Assassination"]: () => number;
 } = {
-  Tracking: () => getRandomInt(5, 75) / 20,
-  "Bounty Hunter": () => getRandomInt(5, 75) / 20,
-  Retirement: () => getRandomInt(5, 75) / 20,
-  Investigation: () => getRandomInt(10, 40) / 20,
-  "Undercover Operation": () => getRandomInt(10, 40) / 20,
-  "Sting Operation": () => getRandomInt(3, 40) / 20,
-  Raid: () => getRandomInt(2, 40) / 20,
-  "Stealth Retirement Operation": () => getRandomInt(1, 20) / 20,
-  Assassination: () => getRandomInt(1, 20) / 20,
+  Tracking: () => getRandomInt(5, 75) / 10,
+  "Bounty Hunter": () => getRandomInt(5, 75) / 10,
+  Retirement: () => getRandomInt(5, 75) / 10,
+  Investigation: () => getRandomInt(10, 40) / 10,
+  "Undercover Operation": () => getRandomInt(10, 40) / 10,
+  "Sting Operation": () => getRandomInt(3, 40) / 10,
+  Raid: () => getRandomInt(2, 40) / 10,
+  "Stealth Retirement Operation": () => getRandomInt(1, 20) / 10,
+  Assassination: () => getRandomInt(1, 20) / 10,
 };
