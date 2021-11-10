77fce118131ad7e2af963c7eb3f58b96ffe00a94
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-11-10 09:23:48 -0500

    lower gang power

--- src/Gang/data/power.ts
+++ src/Gang/data/power.ts
@@ -3,9 +3,9 @@ export const PowerMultiplier: {
 } = {
   "Slum Snakes": 1,
   Tetrads: 2,
-  "The Syndicate": 5,
-  "The Dark Army": 10,
-  "Speakers for the Dead": 50,
-  NiteSec: 10,
-  "The Black Hand": 50,
+  "The Syndicate": 2,
+  "The Dark Army": 2,
+  "Speakers for the Dead": 5,
+  NiteSec: 2,
+  "The Black Hand": 5,
 };
