4730347330e98a2a770df7c65cce895028b4059b
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-11-17 10:59:33 -0500

    revert dev meny thingy

--- src/Sidebar/ui/SidebarRoot.tsx
+++ src/Sidebar/ui/SidebarRoot.tsx
@@ -753,7 +753,7 @@ export function SidebarRoot(props: IProps): React.ReactElement {
               <Typography color={props.page !== Page.Options ? "secondary" : "primary"}>Options</Typography>
             </ListItemText>
           </ListItem>
-          {true && (
+          {process.env.NODE_ENV === "development" && (
             <ListItem
               classes={{ root: classes.listitem }}
               button
