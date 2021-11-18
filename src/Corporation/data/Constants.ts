f3fa2a7c791a9166aa6a3b457776a4c47b4b4b58
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-11-18 11:30:45 -0500

    v1.0.1

--- src/Corporation/data/Constants.ts
+++ src/Corporation/data/Constants.ts
@@ -50,7 +50,7 @@ export const CorporationConstants: {
 
   ProductProductionCostRatio: 5, //Ratio of material cost of a product to its production cost
 
-  DividendMaxPercentage: 0.5,
+  DividendMaxPercentage: 1,
 
   EmployeeSalaryMultiplier: 3, // Employee stats multiplied by this to determine initial salary
   CyclesPerEmployeeRaise: 400, // All employees get a raise every X market cycles
