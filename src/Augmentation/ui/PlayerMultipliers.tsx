c1e3a170abf5efdadb416da59dd6bcec2043ebe7
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-12-13 20:28:21 -0500

    Augmentations page shows stats reductions

--- src/Augmentation/ui/PlayerMultipliers.tsx
+++ src/Augmentation/ui/PlayerMultipliers.tsx
@@ -11,6 +11,7 @@ import TableBody from "@mui/material/TableBody";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { BitNodeMultipliers } from "../../BitNode/BitNodeMultipliers";
 
 function calculateAugmentedStats(): any {
   const augP: any = {};
@@ -24,7 +25,7 @@ function calculateAugmentedStats(): any {
   return augP;
 }
 
-function Improvements({ r }: { r: number }): React.ReactElement {
+function Improvements({ r, m }: { r: number; m: number }): React.ReactElement {
   if (r) {
     return (
       <>
@@ -32,7 +33,9 @@ function Improvements({ r }: { r: number }): React.ReactElement {
           <Typography>&nbsp;{"=>"}&nbsp;</Typography>
         </TableCell>
         <TableCell key="3">
-          <Typography>{numeralWrapper.formatPercentage(r)}</Typography>
+          <Typography>
+            {numeralWrapper.formatPercentage(r)} <BN5Stat base={r} mult={m} />
+          </Typography>
         </TableCell>
       </>
     );
@@ -40,7 +43,17 @@ function Improvements({ r }: { r: number }): React.ReactElement {
   return <></>;
 }
 
-function MultiplierTable({ rows }: { rows: [string, number, number][] }): React.ReactElement {
+interface IBN5StatsProps {
+  base: number;
+  mult: number;
+}
+
+function BN5Stat(props: IBN5StatsProps): React.ReactElement {
+  if (props.mult === 1) return <></>;
+  return <>({numeralWrapper.formatPercentage(props.base * props.mult)})</>;
+}
+
+function MultiplierTable({ rows }: { rows: [string, number, number, number][] }): React.ReactElement {
   return (
     <Table size="small" padding="none">
       <TableBody>
@@ -50,9 +63,11 @@ function MultiplierTable({ rows }: { rows: [string, number, number][] }): React.
               <Typography noWrap>{r[0]} multiplier:&nbsp;</Typography>
             </TableCell>
             <TableCell key="1" style={{ textAlign: "right" }}>
-              <Typography noWrap>{numeralWrapper.formatPercentage(r[1])}</Typography>
+              <Typography noWrap>
+                {numeralWrapper.formatPercentage(r[1])} <BN5Stat base={r[1]} mult={r[3]} />
+              </Typography>
             </TableCell>
-            <Improvements r={r[2]} />
+            <Improvements r={r[2]} m={r[3]} />
           </TableRow>
         ))}
       </TableBody>
@@ -73,21 +88,25 @@ export function PlayerMultipliers(): React.ReactElement {
               "Bladeburner Success Chance",
               Player.bladeburner_success_chance_mult,
               Player.bladeburner_success_chance_mult * mults.bladeburner_success_chance_mult,
+              1,
             ],
             [
               "Bladeburner Max Stamina",
               Player.bladeburner_max_stamina_mult,
               Player.bladeburner_max_stamina_mult * mults.bladeburner_max_stamina_mult,
+              1,
             ],
             [
               "Bladeburner Stamina Gain",
               Player.bladeburner_stamina_gain_mult,
               Player.bladeburner_stamina_gain_mult * mults.bladeburner_stamina_gain_mult,
+              1,
             ],
             [
               "Bladeburner Field Analysis",
               Player.bladeburner_analysis_mult,
               Player.bladeburner_analysis_mult * mults.bladeburner_analysis_mult,
+              1,
             ],
           ]}
         />
@@ -102,58 +121,98 @@ export function PlayerMultipliers(): React.ReactElement {
       <Box mx={2}>
         <MultiplierTable
           rows={[
-            ["Hacking Chance ", Player.hacking_chance_mult, Player.hacking_chance_mult * mults.hacking_chance_mult],
-            ["Hacking Speed ", Player.hacking_speed_mult, Player.hacking_speed_mult * mults.hacking_speed_mult],
-            ["Hacking Money ", Player.hacking_money_mult, Player.hacking_money_mult * mults.hacking_money_mult],
-            ["Hacking Growth ", Player.hacking_grow_mult, Player.hacking_grow_mult * mults.hacking_grow_mult],
+            ["Hacking Chance ", Player.hacking_chance_mult, Player.hacking_chance_mult * mults.hacking_chance_mult, 1],
+            ["Hacking Speed ", Player.hacking_speed_mult, Player.hacking_speed_mult * mults.hacking_speed_mult, 1],
+            ["Hacking Money ", Player.hacking_money_mult, Player.hacking_money_mult * mults.hacking_money_mult, 1],
+            ["Hacking Growth ", Player.hacking_grow_mult, Player.hacking_grow_mult * mults.hacking_grow_mult, 1],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Hacking Level ", Player.hacking_mult, Player.hacking_mult * mults.hacking_mult],
-            ["Hacking Experience ", Player.hacking_exp_mult, Player.hacking_exp_mult * mults.hacking_exp_mult],
+            [
+              "Hacking Level ",
+              Player.hacking_mult,
+              Player.hacking_mult * mults.hacking_mult,
+              BitNodeMultipliers.HackingLevelMultiplier,
+            ],
+            [
+              "Hacking Experience ",
+              Player.hacking_exp_mult,
+              Player.hacking_exp_mult * mults.hacking_exp_mult,
+              BitNodeMultipliers.HackExpGain,
+            ],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Strength Level ", Player.strength_mult, Player.strength_mult * mults.strength_mult],
-            ["Strength Experience ", Player.strength_exp_mult, Player.strength_exp_mult * mults.strength_exp_mult],
+            [
+              "Strength Level ",
+              Player.strength_mult,
+              Player.strength_mult * mults.strength_mult,
+              BitNodeMultipliers.StrengthLevelMultiplier,
+            ],
+            ["Strength Experience ", Player.strength_exp_mult, Player.strength_exp_mult * mults.strength_exp_mult, 1],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Defense Level ", Player.defense_mult, Player.defense_mult * mults.defense_mult],
-            ["Defense Experience ", Player.defense_exp_mult, Player.defense_exp_mult * mults.defense_exp_mult],
+            [
+              "Defense Level ",
+              Player.defense_mult,
+              Player.defense_mult * mults.defense_mult,
+              BitNodeMultipliers.DefenseLevelMultiplier,
+            ],
+            ["Defense Experience ", Player.defense_exp_mult, Player.defense_exp_mult * mults.defense_exp_mult, 1],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Dexterity Level ", Player.dexterity_mult, Player.dexterity_mult * mults.dexterity_mult],
-            ["Dexterity Experience ", Player.dexterity_exp_mult, Player.dexterity_exp_mult * mults.dexterity_exp_mult],
+            [
+              "Dexterity Level ",
+              Player.dexterity_mult,
+              Player.dexterity_mult * mults.dexterity_mult,
+              BitNodeMultipliers.DexterityLevelMultiplier,
+            ],
+            [
+              "Dexterity Experience ",
+              Player.dexterity_exp_mult,
+              Player.dexterity_exp_mult * mults.dexterity_exp_mult,
+              1,
+            ],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Agility Level ", Player.agility_mult, Player.agility_mult * mults.agility_mult],
-            ["Agility Experience ", Player.agility_exp_mult, Player.agility_exp_mult * mults.agility_exp_mult],
+            [
+              "Agility Level ",
+              Player.agility_mult,
+              Player.agility_mult * mults.agility_mult,
+              BitNodeMultipliers.AgilityLevelMultiplier,
+            ],
+            ["Agility Experience ", Player.agility_exp_mult, Player.agility_exp_mult * mults.agility_exp_mult, 1],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Charisma Level ", Player.charisma_mult, Player.charisma_mult * mults.charisma_mult],
-            ["Charisma Experience ", Player.charisma_exp_mult, Player.charisma_exp_mult * mults.charisma_exp_mult],
+            [
+              "Charisma Level ",
+              Player.charisma_mult,
+              Player.charisma_mult * mults.charisma_mult,
+              BitNodeMultipliers.CharismaLevelMultiplier,
+            ],
+            ["Charisma Experience ", Player.charisma_exp_mult, Player.charisma_exp_mult * mults.charisma_exp_mult, 1],
           ]}
         />
         <br />
@@ -164,26 +223,31 @@ export function PlayerMultipliers(): React.ReactElement {
               "Hacknet Node production ",
               Player.hacknet_node_money_mult,
               Player.hacknet_node_money_mult * mults.hacknet_node_money_mult,
+              BitNodeMultipliers.HacknetNodeMoney,
             ],
             [
               "Hacknet Node purchase cost ",
               Player.hacknet_node_purchase_cost_mult,
               Player.hacknet_node_purchase_cost_mult * mults.hacknet_node_purchase_cost_mult,
+              1,
             ],
             [
               "Hacknet Node RAM upgrade cost ",
               Player.hacknet_node_ram_cost_mult,
               Player.hacknet_node_ram_cost_mult * mults.hacknet_node_ram_cost_mult,
+              1,
             ],
             [
               "Hacknet Node Core purchase cost ",
               Player.hacknet_node_core_cost_mult,
               Player.hacknet_node_core_cost_mult * mults.hacknet_node_core_cost_mult,
+              1,
             ],
             [
               "Hacknet Node level upgrade cost ",
               Player.hacknet_node_level_cost_mult,
               Player.hacknet_node_level_cost_mult * mults.hacknet_node_level_cost_mult,
+              1,
             ],
           ]}
         />
@@ -191,17 +255,32 @@ export function PlayerMultipliers(): React.ReactElement {
 
         <MultiplierTable
           rows={[
-            ["Company reputation gain ", Player.company_rep_mult, Player.company_rep_mult * mults.company_rep_mult],
-            ["Faction reputation gain ", Player.faction_rep_mult, Player.faction_rep_mult * mults.faction_rep_mult],
-            ["Salary ", Player.work_money_mult, Player.work_money_mult * mults.work_money_mult],
+            ["Company reputation gain ", Player.company_rep_mult, Player.company_rep_mult * mults.company_rep_mult, 1],
+            [
+              "Faction reputation gain ",
+              Player.faction_rep_mult,
+              Player.faction_rep_mult * mults.faction_rep_mult,
+              BitNodeMultipliers.FactionWorkRepGain,
+            ],
+            [
+              "Salary ",
+              Player.work_money_mult,
+              Player.work_money_mult * mults.work_money_mult,
+              BitNodeMultipliers.CompanyWorkMoney,
+            ],
           ]}
         />
         <br />
 
         <MultiplierTable
           rows={[
-            ["Crime success ", Player.crime_success_mult, Player.crime_success_mult * mults.crime_success_mult],
-            ["Crime money ", Player.crime_money_mult, Player.crime_money_mult * mults.crime_money_mult],
+            ["Crime success ", Player.crime_success_mult, Player.crime_success_mult * mults.crime_success_mult, 1],
+            [
+              "Crime money ",
+              Player.crime_money_mult,
+              Player.crime_money_mult * mults.crime_money_mult,
+              BitNodeMultipliers.CrimeMoney,
+            ],
           ]}
         />
         <br />
