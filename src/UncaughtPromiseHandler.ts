009bae5870e21cd22c160c47c84e929cec544ab9
Olivier Gagnon <oli.gagnon4418@gmail.com>
2021-12-20 16:06:19 -0500

    catch scripts taht end too early

--- src/UncaughtPromiseHandler.ts
+++ src/UncaughtPromiseHandler.ts
@@ -1,3 +1,4 @@
+import { WorkerScript } from "./Netscript/WorkerScript";
 import { isScriptErrorMessage } from "./NetscriptEvaluator";
 import { dialogBoxCreate } from "./ui/React/DialogBox";
 
@@ -12,6 +13,11 @@ export function setupUncaughtPromiseHandler(): void {
       let msg = `UNCAUGHT PROMISE ERROR<br>You forgot to await a promise<br>${scriptName}@${hostname}<br>`;
       msg += "<br>";
       msg += errorMsg;
+      dialogBoxCreate(msg);
+    } else if (e.reason instanceof WorkerScript) {
+      const msg =
+        `UNCAUGHT PROMISE ERROR<br>You forgot to await a promise<br>${e.reason.name}@${e.reason.hostname}<br>` +
+        `Maybe hack / grow / weaken ?`;
       dialogBoxCreate(msg);
     }
   });
