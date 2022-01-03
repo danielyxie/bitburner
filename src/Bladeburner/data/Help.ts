88db255f293a29b0f3496464908fb43b2e3deefd
nickofolas <nickofolas@gmail.com>
2022-01-03 16:42:49 -0600

    Fix whitespace in Bladeburner console

--- src/Bladeburner/data/Help.ts
+++ src/Bladeburner/data/Help.ts
@@ -25,8 +25,8 @@ export const ConsoleHelpText: {
     "automate [var] [val] [hi/low]",
     "",
     "A simple way to automate your Bladeburner actions. This console command can be used " +
-      "to automatically start an action when your stamina rises above a certain threshold, and " +
-      "automatically switch to another action when your stamina drops below another threshold.",
+    "to automatically start an action when your stamina rises above a certain threshold, and " +
+    "automatically switch to another action when your stamina drops below another threshold.",
     "    automate status - Check the current status of your automation and get a brief description of what it'll do",
     "    automate en - Enable the automation feature",
     "    automate dis - Disable the automation feature",
@@ -39,9 +39,9 @@ export const ConsoleHelpText: {
     "    automate general 'Field Analysis' low",
     "",
     "Using the four console commands above will set the automation to perform Tracking contracts " +
-      "if your stamina is 100 or higher, and then switch to Field Analysis if your stamina drops below " +
-      "50. Note that when setting the action, the name of the action is CASE-SENSITIVE. It must " +
-      "exactly match whatever the name is in the UI.",
+    "if your stamina is 100 or higher, and then switch to Field Analysis if your stamina drops below " +
+    "50. Note that when setting the action, the name of the action is CASE-SENSITIVE. It must " +
+    "exactly match whatever the name is in the UI.",
   ],
   clear: ["clear", "", "Clears the console"],
   cls: ["cls", "", "Clears the console"],
@@ -49,8 +49,8 @@ export const ConsoleHelpText: {
     "help [command]",
     "",
     "Running 'help' with no arguments displays the general help text, which lists all console commands " +
-      "and a brief description of what they do. A command can be specified to get more specific help text " +
-      "about that particular command. For example:",
+    "and a brief description of what they do. A command can be specified to get more specific help text " +
+    "about that particular command. For example:",
     "",
     "    help automate",
     "",
@@ -60,8 +60,8 @@ export const ConsoleHelpText: {
     "log [en/dis] [type]",
     "",
     "Enable or disable logging. By default, the results of completing actions such as contracts/operations are logged " +
-      "in the console. There are also random events that are logged in the console as well. The five categories of " +
-      "things that get logged are:",
+    "in the console. There are also random events that are logged in the console as well. The five categories of " +
+    "things that get logged are:",
     "",
     "[general, contracts, ops, blackops, events]",
     "",
@@ -86,10 +86,11 @@ export const ConsoleHelpText: {
     "    skill list",
     "",
     "To display information about a specific skill, specify the name of the skill afterwards. " +
-      "Note that the name of the skill is case-sensitive. Enter it exactly as seen in the UI. If " +
-      "the name of the skill has whitespace, enclose the name of the skill in double quotation marks:",
+    "Note that the name of the skill is case-sensitive. Enter it exactly as seen in the UI. If " +
+    "the name of the skill has whitespace, enclose the name of the skill in double quotation marks:",
     "",
-    "    skill list Reaper<br>" + "    skill list 'Digital Observer'",
+    "    skill list Reaper",
+    "    skill list 'Digital Observer'",
     "",
     "This console command can also be used to level up skills:",
     "",
@@ -99,9 +100,9 @@ export const ConsoleHelpText: {
     "start [type] [name]",
     "",
     "Start an action. An action is specified by its type and its name. The " +
-      "name is case-sensitive. It must appear exactly as it does in the UI. If " +
-      "the name of the action has whitespace, enclose it in double quotation marks. " +
-      "Valid action types include:",
+    "name is case-sensitive. It must appear exactly as it does in the UI. If " +
+    "the name of the action has whitespace, enclose it in double quotation marks. " +
+    "Valid action types include:",
     "",
     "[general, contract, op, blackop]",
     "",
