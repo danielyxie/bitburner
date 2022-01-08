a9719214179e1519d5d153d3ca67da326c0317b2
nickofolas <nickofolas@gmail.com>
2022-01-08 14:56:29 -0600

    Document null duration for toasts

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -5670,9 +5670,9 @@ export interface NS extends Singularity {
    * Queue a toast (bottom-right notification).
    * @param msg - Message in the toast.
    * @param variant - Type of toast, must be one of success, info, warning, error. Defaults to success.
-   * @param duration - Duration of toast in ms, defaults to 2000
+   * @param duration - Duration of toast in ms. Can also be null to create a persistent toast. Defaults to 2000
    */
-  toast(msg: any, variant?: string, duration?: number): void;
+  toast(msg: any, variant?: string, duration?: number | null): void;
 
   /**
    * Download a file from the internet.
