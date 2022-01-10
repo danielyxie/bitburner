30f6d157d9698a59d705c8a4644cde4c3d7aae40
nickofolas <nickofolas@gmail.com>
2022-01-10 14:15:32 -0600

    Fix indentation

--- src/Company/formulas/favor.ts
+++ src/Company/formulas/favor.ts
@@ -3,12 +3,11 @@
 // for information on how to calculate this
 
 export function favorToRep(f: number): number {
-    const raw = 25000 * (Math.pow(1.02, f) - 1);
-    return Math.round(raw * 10000) / 10000; // round to make things easier.
-  }
-  
-  export function repToFavor(r: number): number {
-    const raw = Math.log(r / 25000 + 1) / Math.log(1.02);
-    return Math.round(raw * 10000) / 10000; // round to make things easier.
-  }
-  
+  const raw = 25000 * (Math.pow(1.02, f) - 1);
+  return Math.round(raw * 10000) / 10000; // round to make things easier.
+}
+
+export function repToFavor(r: number): number {
+  const raw = Math.log(r / 25000 + 1) / Math.log(1.02);
+  return Math.round(raw * 10000) / 10000; // round to make things easier.
+}
