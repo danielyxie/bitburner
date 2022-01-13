ea88f5f150ef369387187c8608e18fd150c58bbd
pigalot <pigalot@users.noreply.github.com>
2022-01-13 19:33:59 +0000

    spelling mistake

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -6120,7 +6120,7 @@ export interface OfficeAPI {
    */
   hireAdVert(divisionName: string): void;
   /**
-   * purchace a research
+   * Purchase a research
    * @param divisionName - Name of the division
    * @param researchName - Name of the research
    */
