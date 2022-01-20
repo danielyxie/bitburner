cf111e8c02c492af47bef97063e6daa2eb4ca668
Martin Fournier <martin@mfournier.com>
2022-01-20 18:43:36 -0500

    Add ID to ThemeEditorButton

--- src/Themes/ui/ThemeEditorButton.tsx
+++ src/Themes/ui/ThemeEditorButton.tsx
@@ -14,7 +14,7 @@ export function ThemeEditorButton({ router }: IProps): React.ReactElement {
   return (
     <>
       <Tooltip title="The theme editor allows you to modify the colors the game uses.">
-        <Button startIcon={<ColorizeIcon />} onClick={() => setThemeEditorOpen(true)}>
+        <Button id="bb-theme-editor-button" startIcon={<ColorizeIcon />} onClick={() => setThemeEditorOpen(true)}>
           Theme Editor
         </Button>
       </Tooltip>
