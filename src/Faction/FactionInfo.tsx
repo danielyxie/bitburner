f010d088618a0c004903ca5e2217c447fdc21c9f
jjayeon <autumn.chiu.136@gmail.com>
2022-03-01 10:46:17 -0500

    sort FactionInfo.tsx by canonical order

--- src/Faction/FactionInfo.tsx
+++ src/Faction/FactionInfo.tsx
@@ -89,12 +89,147 @@ export class FactionInfo {
  */
 // tslint:disable-next-line:variable-name
 export const FactionInfos: IMap<FactionInfo> = {
-  // Endgame
-  Illuminati: new FactionInfo(
+
+  // Earlygame factions - factions the player will prestige with early on that don't belong in other categories.
+  CyberSec: new FactionInfo(
     (
       <>
-        Humanity never changes. No matter how civilized society becomes, it will eventually fall back into chaos. And
-        from this chaos, we are the invisible hand that guides them to order.{" "}
+        The Internet is the first thing that was built that we don't fully understand, the largest experiment in anarchy
+        that we have ever had. And as the world becomes increasingly dominated by it, society approaches the brink of
+        total chaos. We serve only to protect society, to protect humanity, to protect the world from imminent collapse.
+      </>
+    ),
+    [],
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+  ),
+
+  "Tian Di Hui": new FactionInfo(<>Obey Heaven and work righteously.</>, [], true, true, false, true, false, false),
+
+  Netburners: new FactionInfo(<>{"~~//*>H4CK||3T 8URN3R5**>?>\\~~"}</>, [], true, true, false, false, false, false),
+
+  // City factions, essentially governments
+  "Sector-12": new FactionInfo(
+    <>The City of the Future.</>,
+    ["Chongqing", "New Tokyo", "Ishima", "Volhaven"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+  Chongqing: new FactionInfo(
+    <>Serve the People.</>,
+    ["Sector-12", "Aevum", "Volhaven"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+  "New Tokyo": new FactionInfo(
+    <>Asia's World City.</>,
+    ["Sector-12", "Aevum", "Volhaven"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+  Ishima: new FactionInfo(
+    <>The East Asian Order of the Future.</>,
+    ["Sector-12", "Aevum", "Volhaven"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+  Aevum: new FactionInfo(
+    <>The Silicon City.</>,
+    ["Chongqing", "New Tokyo", "Ishima", "Volhaven"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+  Volhaven: new FactionInfo(
+    <>Benefit, Honor, and Glory.</>,
+    ["Chongqing", "Sector-12", "New Tokyo", "Aevum", "Ishima"],
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+  ),
+
+  // Hacker groups
+
+  // prettier-ignore
+  NiteSec: new FactionInfo(<>
+    {"                  __..__               "}<br />
+    {"                _.nITESECNIt.            "}<br />
+    {"             .-'NITESECNITESEc.          "}<br />
+    {"           .'    NITESECNITESECn         "}<br />
+    {"          /       NITESECNITESEC;        "}<br />
+    {"         :        :NITESECNITESEC;       "}<br />
+    {"         ;       $ NITESECNITESECN       "}<br />
+    {"        :    _,   ,N'ITESECNITESEC       "}<br />
+    {"        : .+^^`,  :    `NITESECNIT       "}<br />
+    {"         ) /),     `-,-=,NITESECNI       "}<br />
+    {"        /  ^         ,-;|NITESECN;       "}<br />
+    {"       /     _.'     '-';NITESECN        "}<br />
+    {"      (  ,           ,-''`^NITE'         "}<br />
+    {"       )`            :`.    .'           "}<br />
+    {"       )--           ;  `- /             "}<br />
+    {"       '        _.-'     :              "}<br />
+    {"       (     _.-'   .                  "}<br />
+    {"        ------.                       "}<br />
+    {"                .                     "}<br />
+    {"                         _.nIt          "}<br />
+    {"                    _.nITESECNi         "}<br />
+    {"                   nITESECNIT^'         "}<br />
+    {"                   NITE^' ___           "}<br />
+    {"                  /    .gP''''Tp.       "}<br />
+    {"                 :    d'     .  `b      "}<br />
+    {"                 ;   d'       o  `b ;    "}<br />
+    {"                /   d;            `b|    "}<br />
+    {"               /,   $;          @  `:    "}<br />
+    {"              /'    $/               ;   "}<br />
+    {"            .'      $/b          o   |   "}<br />
+    {"          .'       d$/$;             :   "}<br />
+    {"         /       .d/$/$;          ,   ;  "}<br />
+    {"        d      .dNITESEC          $   |  "}<br />
+    {"       :bp.__.gNITESEC/$         :$   ;  "}<br />
+    {"       NITESECNITESECNIT         /$b :   "}<br /></>,
+    [],
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+  ),
+  "The Black Hand": new FactionInfo(
+    (
+      <>
+        The world, so afraid of strong government, now has no government. Only power - Digital power. Financial power.
+        Technological power. And those at the top rule with an invisible hand. They built a society where the rich get
+        richer, and everyone else suffers.
+        <br />
+        <br />
+        So much pain. So many lives. Their darkness must end.
       </>
     ),
     [],
@@ -105,37 +240,27 @@ export const FactionInfos: IMap<FactionInfo> = {
     false,
     false,
   ),
-
-  Daedalus: new FactionInfo(
-    <>Yesterday we obeyed kings and bent our necks to emperors. Today we kneel only to truth.</>,
-    [],
-    true,
-    true,
-    true,
-    false,
-    false,
-    false,
-  ),
-
-  "The Covenant": new FactionInfo(
+  BitRunners: new FactionInfo(
     (
       <>
-        Surrender yourself. Give up your empty individuality to become part of something great, something eternal.
-        Become a slave. Submit your mind, body, and soul. Only then can you set yourself free.
+        Our entire lives are controlled by bits. All of our actions, our thoughts, our personal information. It's all
+        transformed into bits, stored in bits, communicated through bits. It’s impossible for any person to move, to
+        live, to operate at any level without the use of bits. And when a person moves, lives, and operates, they leave
+        behind their bits, mere traces of seemingly meaningless fragments of information. But these bits can be
+        reconstructed. Transformed. Used.
         <br />
         <br />
-        Only then can you discover immortality.
+        Those who run the bits, run the world.
       </>
     ),
     [],
     true,
     true,
-    true,
+    false,
     false,
     false,
     false,
   ),
-
   // Megacorporations, each forms its own faction
   ECorp: new FactionInfo(
     (
@@ -173,56 +298,8 @@ export const FactionInfos: IMap<FactionInfo> = {
     true,
   ),
 
-  "Bachman & Associates": new FactionInfo(
-    (
-      <>
-        Where Law and Business meet - thats where we are.
-        <br />
-        <br />
-        Legal Insight - Business Instinct - Innovative Experience.
-      </>
-    ),
-    [],
-    true,
-    true,
-    true,
-    true,
-    false,
-    true,
-  ),
-
-  "Blade Industries": new FactionInfo(<>Augmentation is Salvation.</>, [], true, true, true, true, false, true),
-
-  NWO: new FactionInfo(
-    (
-      <>
-        Humans don't truly desire freedom. They want to be observed, understood, and judged. They want to be given
-        purpose and direction in life. That is why they created God. And that is why they created civilization - not
-        because of willingness, but because of a need to be incorporated into higher orders of structure and meaning.
-      </>
-    ),
-    [],
-    true,
-    true,
-    true,
-    true,
-    false,
-    true,
-  ),
-
-  "Clarke Incorporated": new FactionInfo(
-    <>The Power of the Genome - Unlocked.</>,
-    [],
-    true,
-    true,
-    true,
-    true,
-    false,
-    true,
-  ),
-
-  "OmniTek Incorporated": new FactionInfo(
-    <>Simply put, our mission is to design and build robots that make a difference.</>,
+  "KuaiGong International": new FactionInfo(
+    <>Dream big. Work hard. Make history.</>,
     [],
     true,
     true,
@@ -248,8 +325,56 @@ export const FactionInfos: IMap<FactionInfo> = {
     true,
   ),
 
-  "KuaiGong International": new FactionInfo(
-    <>Dream big. Work hard. Make history.</>,
+  NWO: new FactionInfo(
+    (
+      <>
+        Humans don't truly desire freedom. They want to be observed, understood, and judged. They want to be given
+        purpose and direction in life. That is why they created God. And that is why they created civilization - not
+        because of willingness, but because of a need to be incorporated into higher orders of structure and meaning.
+      </>
+    ),
+    [],
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+  ),
+
+  "Blade Industries": new FactionInfo(<>Augmentation is Salvation.</>, [], true, true, true, true, false, true),
+
+  "OmniTek Incorporated": new FactionInfo(
+    <>Simply put, our mission is to design and build robots that make a difference.</>,
+    [],
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+  ),
+
+  "Bachman & Associates": new FactionInfo(
+    (
+      <>
+        Where Law and Business meet - thats where we are.
+        <br />
+        <br />
+        Legal Insight - Business Instinct - Innovative Experience.
+      </>
+    ),
+    [],
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+  ),
+
+  "Clarke Incorporated": new FactionInfo(
+    <>The Power of the Genome - Unlocked.</>,
     [],
     true,
     true,
@@ -276,158 +401,40 @@ export const FactionInfos: IMap<FactionInfo> = {
     true,
   ),
 
-  // Hacker groups
-  BitRunners: new FactionInfo(
-    (
-      <>
-        Our entire lives are controlled by bits. All of our actions, our thoughts, our personal information. It's all
-        transformed into bits, stored in bits, communicated through bits. It’s impossible for any person to move, to
-        live, to operate at any level without the use of bits. And when a person moves, lives, and operates, they leave
-        behind their bits, mere traces of seemingly meaningless fragments of information. But these bits can be
-        reconstructed. Transformed. Used.
-        <br />
-        <br />
-        Those who run the bits, run the world.
-      </>
-    ),
-    [],
-    true,
-    true,
-    false,
-    false,
-    false,
-    false,
-  ),
-
-  "The Black Hand": new FactionInfo(
-    (
-      <>
-        The world, so afraid of strong government, now has no government. Only power - Digital power. Financial power.
-        Technological power. And those at the top rule with an invisible hand. They built a society where the rich get
-        richer, and everyone else suffers.
-        <br />
-        <br />
-        So much pain. So many lives. Their darkness must end.
-      </>
-    ),
-    [],
-    true,
-    true,
-    true,
-    false,
-    false,
-    false,
-  ),
-
-  // prettier-ignore
-  NiteSec: new FactionInfo(<>
-{"                  __..__               "}<br />
-{"                _.nITESECNIt.            "}<br />
-{"             .-'NITESECNITESEc.          "}<br />
-{"           .'    NITESECNITESECn         "}<br />
-{"          /       NITESECNITESEC;        "}<br />
-{"         :        :NITESECNITESEC;       "}<br />
-{"         ;       $ NITESECNITESECN       "}<br />
-{"        :    _,   ,N'ITESECNITESEC       "}<br />
-{"        : .+^^`,  :    `NITESECNIT       "}<br />
-{"         ) /),     `-,-=,NITESECNI       "}<br />
-{"        /  ^         ,-;|NITESECN;       "}<br />
-{"       /     _.'     '-';NITESECN        "}<br />
-{"      (  ,           ,-''`^NITE'         "}<br />
-{"       )`            :`.    .'           "}<br />
-{"       )--           ;  `- /             "}<br />
-{"       '        _.-'     :              "}<br />
-{"       (     _.-'   .                  "}<br />
-{"        ------.                       "}<br />
-{"                .                     "}<br />
-{"                         _.nIt          "}<br />
-{"                    _.nITESECNi         "}<br />
-{"                   nITESECNIT^'         "}<br />
-{"                   NITE^' ___           "}<br />
-{"                  /    .gP''''Tp.       "}<br />
-{"                 :    d'     .  `b      "}<br />
-{"                 ;   d'       o  `b ;    "}<br />
-{"                /   d;            `b|    "}<br />
-{"               /,   $;          @  `:    "}<br />
-{"              /'    $/               ;   "}<br />
-{"            .'      $/b          o   |   "}<br />
-{"          .'       d$/$;             :   "}<br />
-{"         /       .d/$/$;          ,   ;  "}<br />
-{"        d      .dNITESEC          $   |  "}<br />
-{"       :bp.__.gNITESEC/$         :$   ;  "}<br />
-{"       NITESECNITESECNIT         /$b :   "}<br /></>,
-    [],
-    true,
-    true,
-    false,
-    false,
-    false,
-    false,
-  ),
-
-  // City factions, essentially governments
-  Aevum: new FactionInfo(
-    <>The Silicon City.</>,
-    ["Chongqing", "New Tokyo", "Ishima", "Volhaven"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-  Chongqing: new FactionInfo(
-    <>Serve the People.</>,
-    ["Sector-12", "Aevum", "Volhaven"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-  Ishima: new FactionInfo(
-    <>The East Asian Order of the Future.</>,
-    ["Sector-12", "Aevum", "Volhaven"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-  "New Tokyo": new FactionInfo(
-    <>Asia's World City.</>,
-    ["Sector-12", "Aevum", "Volhaven"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-  "Sector-12": new FactionInfo(
-    <>The City of the Future.</>,
-    ["Chongqing", "New Tokyo", "Ishima", "Volhaven"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-  Volhaven: new FactionInfo(
-    <>Benefit, Honor, and Glory.</>,
-    ["Chongqing", "Sector-12", "New Tokyo", "Aevum", "Ishima"],
-    true,
-    true,
-    true,
-    true,
-    false,
-    false,
-  ),
-
   // Criminal Organizations/Gangs
+
+  "Slum Snakes": new FactionInfo(<>Slum Snakes rule!</>, [], false, false, true, true, false, false),
+
+  Tetrads: new FactionInfo(
+    <>Following the mandate of Heaven and carrying out the way.</>,
+    [],
+    false,
+    false,
+    true,
+    true,
+    false,
+    false,
+  ),
+
+  Silhouette: new FactionInfo(
+    (
+      <>
+        Corporations have filled the void of power left behind by the collapse of Western government. The issue is
+        they've become so big that you don't know who they're working for. And if you're employed at one of these
+        corporations, you don't even know who you're working for.
+        <br />
+        <br />
+        That's terror. Terror, fear, and corruption. All born into the system, all propagated by the system.
+      </>
+    ),
+    [],
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+  ),
   "Speakers for the Dead": new FactionInfo(
     <>It is better to reign in Hell than to serve in Heaven.</>,
     [],
@@ -452,15 +459,16 @@ export const FactionInfos: IMap<FactionInfo> = {
 
   "The Syndicate": new FactionInfo(<>Honor holds you back.</>, [], true, true, true, true, false, false),
 
-  Silhouette: new FactionInfo(
+  // Endgame
+
+  "The Covenant": new FactionInfo(
     (
       <>
-        Corporations have filled the void of power left behind by the collapse of Western government. The issue is
-        they've become so big that you don't know who they're working for. And if you're employed at one of these
-        corporations, you don't even know who you're working for.
+        Surrender yourself. Give up your empty individuality to become part of something great, something eternal.
+        Become a slave. Submit your mind, body, and soul. Only then can you set yourself free.
         <br />
         <br />
-        That's terror. Terror, fear, and corruption. All born into the system, all propagated by the system.
+        Only then can you discover immortality.
       </>
     ),
     [],
@@ -472,36 +480,27 @@ export const FactionInfos: IMap<FactionInfo> = {
     false,
   ),
 
-  Tetrads: new FactionInfo(
-    <>Following the mandate of Heaven and carrying out the way.</>,
+  Daedalus: new FactionInfo(
+    <>Yesterday we obeyed kings and bent our necks to emperors. Today we kneel only to truth.</>,
     [],
-    false,
-    false,
     true,
     true,
+    true,
+    false,
     false,
     false,
   ),
-
-  "Slum Snakes": new FactionInfo(<>Slum Snakes rule!</>, [], false, false, true, true, false, false),
-
-  // Earlygame factions - factions the player will prestige with early on that don't belong in other categories.
-  Netburners: new FactionInfo(<>{"~~//*>H4CK||3T 8URN3R5**>?>\\~~"}</>, [], true, true, false, false, false, false),
-
-  "Tian Di Hui": new FactionInfo(<>Obey Heaven and work righteously.</>, [], true, true, false, true, false, false),
-
-  CyberSec: new FactionInfo(
+  Illuminati: new FactionInfo(
     (
       <>
-        The Internet is the first thing that was built that we don't fully understand, the largest experiment in anarchy
-        that we have ever had. And as the world becomes increasingly dominated by it, society approaches the brink of
-        total chaos. We serve only to protect society, to protect humanity, to protect the world from imminent collapse.
+        Humanity never changes. No matter how civilized society becomes, it will eventually fall back into chaos. And
+        from this chaos, we are the invisible hand that guides them to order.{" "}
       </>
     ),
     [],
     true,
     true,
-    false,
+    true,
     false,
     false,
     false,
@@ -558,8 +557,8 @@ export const FactionInfos: IMap<FactionInfo> = {
     {"     -smNNNNmdo-             "}<br />
     {"        `..`                 "}<br /><br />
     Many cultures predict an end to humanity in the near future, a final
-     Armageddon that will end the world; but we disagree.
-    <br /><br />Note that for this faction, reputation can 
+    Armageddon that will end the world; but we disagree.
+    <br /><br />Note that for this faction, reputation can
     only be gained by charging Stanek's gift.</>,
     [],
     false,
