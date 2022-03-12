3cbb926fd853e73fe1b72b678dba9029f586ba37
phyzical <phyzicaly@hotmail.com>
2022-03-12 12:36:47 +0800

    expose the inBladeburner on the player object

--- src/NetscriptFunctions.ts
+++ src/NetscriptFunctions.ts
@@ -2273,6 +2273,7 @@ export function NetscriptFunctions(workerScript: WorkerScript): NS {
         jobs: {},
         factions: Player.factions.slice(),
         tor: Player.hasTorRouter(),
+        inBladeburner: Player.inBladeburner(),
         hasCorporation: Player.hasCorporation(),
       };
       Object.assign(data.jobs, Player.jobs);
