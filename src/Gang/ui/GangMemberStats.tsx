edb347e56613097444246d20035f3dd3d3b9151d
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-03-20 21:22:21 -0400

    rm current task from gang

--- src/Gang/ui/GangMemberStats.tsx
+++ src/Gang/ui/GangMemberStats.tsx
@@ -7,12 +7,7 @@ import { useGang } from "./Context";
 
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableRow,
-} from "@mui/material";
+import { Table, TableBody, TableCell, TableRow } from "@mui/material";
 
 import { numeralWrapper } from "../../ui/numeralFormat";
 import { GangMember } from "../GangMember";
@@ -37,11 +32,8 @@ export function GangMemberStats(props: IProps): React.ReactElement {
     cha: props.member.calculateAscensionMult(props.member.cha_asc_points),
   };
 
-
-
   const gang = useGang();
   const data = [
-    [`Current Task:`, `${props.member.task}`],
     [`Money:`, <MoneyRate money={5 * props.member.calculateMoneyGain(gang)} />],
     [`Respect:`, `${numeralWrapper.formatRespect(5 * props.member.calculateRespectGain(gang))} / sec`],
     [`Wanted Level:`, `${numeralWrapper.formatWanted(5 * props.member.calculateWantedLevelGain(gang))} / sec`],
@@ -79,14 +71,38 @@ export function GangMemberStats(props: IProps): React.ReactElement {
           </Typography>
         }
       >
-        <Table sx={{ display: 'table', mb: 1, width: '100%' }}>
+        <Table sx={{ display: "table", mb: 1, width: "100%" }}>
           <TableBody>
-            <StatsRow name="Hacking" color={Settings.theme.hack} data={{ level: props.member.hack, exp: props.member.hack_exp }} />
-            <StatsRow name="Strength" color={Settings.theme.combat} data={{ level: props.member.str, exp: props.member.str_exp }} />
-            <StatsRow name="Defense" color={Settings.theme.combat} data={{ level: props.member.def, exp: props.member.def_exp }} />
-            <StatsRow name="Dexterity" color={Settings.theme.combat} data={{ level: props.member.dex, exp: props.member.dex_exp }} />
-            <StatsRow name="Agility" color={Settings.theme.combat} data={{ level: props.member.agi, exp: props.member.agi_exp }} />
-            <StatsRow name="Charisma" color={Settings.theme.cha} data={{ level: props.member.cha, exp: props.member.cha_exp }} />
+            <StatsRow
+              name="Hacking"
+              color={Settings.theme.hack}
+              data={{ level: props.member.hack, exp: props.member.hack_exp }}
+            />
+            <StatsRow
+              name="Strength"
+              color={Settings.theme.combat}
+              data={{ level: props.member.str, exp: props.member.str_exp }}
+            />
+            <StatsRow
+              name="Defense"
+              color={Settings.theme.combat}
+              data={{ level: props.member.def, exp: props.member.def_exp }}
+            />
+            <StatsRow
+              name="Dexterity"
+              color={Settings.theme.combat}
+              data={{ level: props.member.dex, exp: props.member.dex_exp }}
+            />
+            <StatsRow
+              name="Agility"
+              color={Settings.theme.combat}
+              data={{ level: props.member.agi, exp: props.member.agi_exp }}
+            />
+            <StatsRow
+              name="Charisma"
+              color={Settings.theme.cha}
+              data={{ level: props.member.cha, exp: props.member.cha_exp }}
+            />
             <TableRow>
               <TableCell classes={{ root: classes.cellNone }}>
                 <br />
