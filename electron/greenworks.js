881ad83690ed1f45124272a81f2cc34b34cf5d41
Master-Guy <566429+Master-Guy@users.noreply.github.com>
2022-03-21 17:24:32 +0100

    Readability

--- electron/greenworks.js
+++ electron/greenworks.js
@@ -8,14 +8,23 @@ var fs = require("fs");
 var greenworks;
 
 if (process.platform == "darwin") {
-  if (process.arch == "x64") greenworks = require("./lib/greenworks-osx64");
-  else if (process.arch == "ia32") greenworks = require("./lib/greenworks-osx32");
+  if (process.arch == "x64") {
+    greenworks = require("./lib/greenworks-osx64");
+  } else if (process.arch == "ia32") {
+    greenworks = require("./lib/greenworks-osx32");
+  }
 } else if (process.platform == "win32") {
-  if (process.arch == "x64") greenworks = require("./lib/greenworks-win64");
-  else if (process.arch == "ia32") greenworks = require("./lib/greenworks-win32");
+  if (process.arch == "x64") {
+    greenworks = require("./lib/greenworks-win64");
+  } else if (process.arch == "ia32") {
+    greenworks = require("./lib/greenworks-win32");
+  }
 } else if (process.platform == "linux") {
-  if (process.arch == "x64") greenworks = require("./lib/greenworks-linux64");
-  else if (process.arch == "ia32") greenworks = require("./lib/greenworks-linux32");
+  if (process.arch == "x64") {
+    greenworks = require("./lib/greenworks-linux64");
+  } else if (process.arch == "ia32") {
+    greenworks = require("./lib/greenworks-linux32");
+  }
 }
 
 function error_process(err, error_callback) {
