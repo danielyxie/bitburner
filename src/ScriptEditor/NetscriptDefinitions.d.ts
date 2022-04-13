729fdf7510e6e45589737e68679c46d78deb5c02
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-04-13 01:00:00 -0400

    fk it

--- src/ScriptEditor/NetscriptDefinitions.d.ts
+++ src/ScriptEditor/NetscriptDefinitions.d.ts
@@ -6422,6 +6422,19 @@ export interface NS {
    * RAM cost: 0.2 GB
    */
   getSharePower(): number;
+
+  // enum: NSEnums;
+}
+
+export enum ToastVariant {
+  SUCCESS = "success",
+  WARNING = "warning",
+  ERROR = "error",
+  INFO = "info",
+}
+
+export interface NSEnums {
+  toast: ToastVariant;
 }
 
 /**
