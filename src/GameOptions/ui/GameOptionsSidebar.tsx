da18115b800da6d89b37466c285e07d63061ed9c
nickofolas <nickofolas@gmail.com>
2022-04-17 15:08:40 -0500

    Sidebar updates

--- src/GameOptions/ui/GameOptionsSidebar.tsx
+++ src/GameOptions/ui/GameOptionsSidebar.tsx
@@ -1,5 +1,17 @@
-import { Download, Palette, Save, Upload } from "@mui/icons-material";
-import { Box, Button, Link, List, ListItemButton, Paper, Tooltip, Typography } from "@mui/material";
+import {
+  BugReport,
+  Chat,
+  Download,
+  LibraryBooks,
+  Palette,
+  Public,
+  Reddit,
+  Save,
+  SystemUpdateAlt,
+  Upload,
+  Bloodtype,
+} from "@mui/icons-material";
+import { Box, Button, List, ListItemButton, Paper, Tooltip, Typography } from "@mui/material";
 import { default as React, useRef, useState } from "react";
 import { FileDiagnosticModal } from "../../Diagnostic/FileDiagnosticModal";
 import { IPlayer } from "../../PersonObjects/IPlayer";
@@ -101,7 +113,7 @@ export const GameOptionsSidebar = (props: IProps): React.ReactElement => {
       <Box
         sx={{
           display: "grid",
-          width: "fit-content",
+          width: "100%",
           height: "fit-content",
           gridTemplateAreas: `"save   delete"
       "export import"
@@ -109,14 +121,15 @@ export const GameOptionsSidebar = (props: IProps): React.ReactElement => {
       "reset  diagnose"
       "browse browse"
       "theme  style"
-      "links  links"`,
+      "links  links"
+      "devs   devs"`,
           gridTemplateColumns: "1fr 1fr",
         }}
       >
         <Button onClick={() => props.save()} startIcon={<Save />} sx={{ gridArea: "save" }}>
           Save Game
         </Button>
-        <Box sx={{ gridArea: "delete", "& .MuiButton-root": { width: "100%" } }}>
+        <Box sx={{ gridArea: "delete", "& .MuiButton-root": { height: "100%", width: "100%" } }}>
           <DeleteGameButton />
         </Box>
         <Tooltip title={<Typography>Export your game to a text file.</Typography>}>
@@ -187,7 +200,12 @@ export const GameOptionsSidebar = (props: IProps): React.ReactElement => {
             Force kill all active scripts
           </Button>
         </Tooltip>
-        <SoftResetButton noConfirmation={Settings.SuppressBuyAugmentationConfirmation} onTriggered={props.softReset} />
+        <Box sx={{ gridArea: "reset", "& .MuiButton-root": { height: "100%", width: "100%" } }}>
+          <SoftResetButton
+            noConfirmation={Settings.SuppressBuyAugmentationConfirmation}
+            onTriggered={props.softReset}
+          />
+        </Box>
         <Tooltip
           title={
             <Typography>
@@ -205,32 +223,112 @@ export const GameOptionsSidebar = (props: IProps): React.ReactElement => {
             Theme Browser
           </Button>
         </Tooltip>
-        <Box sx={{ gridArea: "theme", "& .MuiButton-root": { width: "100%" } }}>
+        <Box sx={{ gridArea: "theme", "& .MuiButton-root": { height: "100%", width: "100%" } }}>
           <ThemeEditorButton router={props.router} />
         </Box>
-        <Box sx={{ gridArea: "style", "& .MuiButton-root": { width: "100%" } }}>
+        <Box sx={{ gridArea: "style", "& .MuiButton-root": { height: "100%", width: "100%" } }}>
           <StyleEditorButton />
         </Box>
-        <Box sx={{ gridArea: "links" }}>
-          <Link href="https://github.com/danielyxie/bitburner/issues/new" target="_blank">
-            <Typography>Report bug</Typography>
-          </Link>
-          <Link href="https://bitburner.readthedocs.io/en/latest/changelog.html" target="_blank">
-            <Typography>Changelog</Typography>
-          </Link>
-          <Link href="https://bitburner.readthedocs.io/en/latest/index.html" target="_blank">
-            <Typography>Documentation</Typography>
-          </Link>
-          <Link href="https://discord.gg/TFc3hKD" target="_blank">
-            <Typography>Discord</Typography>
-          </Link>
-          <Link href="https://www.reddit.com/r/bitburner" target="_blank">
-            <Typography>Reddit</Typography>
-          </Link>
-          <Link href="https://plaza.dsolver.ca/games/bitburner" target="_blank">
-            <Typography>Incremental game plaza</Typography>
-          </Link>
+
+        <Box
+          sx={{
+            gridArea: "links",
+            display: "grid",
+            gridTemplateAreas: `"bug changelog"
+        "docs docs"
+        "discord reddit"
+        "plaza plaza"`,
+            gridTemplateColumns: "1fr 1fr",
+            my: 1,
+          }}
+        >
+          <Button
+            startIcon={<BugReport />}
+            href="https://github.com/danielyxie/bitburner/issues/new"
+            target="_blank"
+            sx={{ gridArea: "bug" }}
+          >
+            Report Bug
+          </Button>
+          <Button
+            startIcon={<SystemUpdateAlt />}
+            href="https://bitburner.readthedocs.io/en/latest/changelog.html"
+            target="_blank"
+            sx={{ gridArea: " changelog" }}
+          >
+            Changelog
+          </Button>
+          <Button
+            startIcon={<LibraryBooks />}
+            href="https://bitburner.readthedocs.io/en/latest/index.html"
+            target="_blank"
+            sx={{ gridArea: "docs" }}
+          >
+            Documentation
+          </Button>
+          <Button startIcon={<Chat />} href="https://discord.gg/TFc3hKD" target="_blank" sx={{ gridArea: "discord" }}>
+            Discord
+          </Button>
+          <Button
+            startIcon={<Reddit />}
+            href="https://www.reddit.com/r/bitburner"
+            target="_blank"
+            sx={{ gridArea: "reddit" }}
+          >
+            Reddit
+          </Button>
+          <Button
+            startIcon={<Public />}
+            href="https://plaza.dsolver.ca/games/bitburner"
+            target="_blank"
+            sx={{ gridArea: "plaza" }}
+          >
+            Incremental game plaza
+          </Button>
         </Box>
+
+        {!location.href.startsWith("file://") && (
+          <Box sx={{ gridArea: "devs" }}>
+            <Button sx={{ width: "100%", display: "flex", flexDirection: "column" }}>
+              danielyxie / BigD (Original Dev)
+              <form
+                action="https://www.paypal.com/cgi-bin/webscr"
+                method="post"
+                target="_blank"
+                style={{ display: "block" }}
+              >
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input
+                  type="hidden"
+                  name="encrypted"
+                  value="-----BEGIN PKCS7-----MIIHRwYJKoZIhvcNAQcEoIIHODCCBzQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA2Y2VGE75oWct89z//G2YEJKmzx0uDTXNrpje9ThxmUnBLFZCY+I11Pors7lGRvFqo5okwnu41CfYMPHDxpAgyYyQndMX9pWUX0gLfBMm2BaHwsNBCwt34WmpQqj7TGsQ+aw9NbmkxiJltGnOa+6/gy10mPZAA3HxiieLeCKkGgDELMAkGBSsOAwIaBQAwgcQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI72F1YSzHUd2AgaDMekHU3AKT93Ey9wkB3486bV+ngFSD6VOHrPweH9QATsp+PMe9QM9vmq+s2bGtTbZaYrFqM3M97SnQ0l7IQ5yuOzdZhRdfysu5uJ8dnuHUzq4gLSzqMnZ6/3c+PoHB8AS1nYHUVL4U0+ogZsO1s97IAQyfck9SaoFlxVtqQhkb8752MkQJJvGu3ZQSQGcVC4hFDPk8prXqyq4BU/k/EliwoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTcwNzI1MDExODE2WjAjBgkqhkiG9w0BCQQxFgQUNo8efiZ7sk7nwKM/6B6Z7sU8hIIwDQYJKoZIhvcNAQEBBQAEgYB+JB4vZ/r48815/1HF/xK3+rOx7bPz3kAXmbhW/mkoF4OUbzqMeljvDIA9q/BDdlCLtxFOw9XlftTzv0eZCW/uCIiwu5wTzPIfPY1SI8WHe4cJbP2f2EYxIVs8D7OSirbW4yVa0+gACaLLj0rzIzNN8P/5PxgB03D+jwkcJABqng==-----END PKCS7-----"
+                />
+                <input
+                  type="image"
+                  // src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                  src="https://www.paypalobjects.com/digitalassets/c/website/marketing/apac/C2/logos-buttons/optimize/26_Yellow_PayPal_Pill_Button.png"
+                  name="submit"
+                  alt="PayPal - The safer, easier way to pay online!"
+                />
+              </form>
+            </Button>
+            <Button
+              href="https://www.google.com/search?q=Where+to+donate+blood+near+me%3F"
+              target="_blank"
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              hydroflame (Current Maintainer)
+              <span style={{ display: "flex", alignItems: "center" }}>
+                <Bloodtype sx={{ mb: 0.5 }} />
+                Donate blood!
+              </span>
+            </Button>
+          </Box>
+        )}
       </Box>
       <FileDiagnosticModal open={diagnosticOpen} onClose={() => setDiagnosticOpen(false)} />
     </Box>
