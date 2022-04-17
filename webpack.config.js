01d7ac6967781154506cd158cbafc38b9023b2d4
JP Sugarbroad <taralx@gmail.com>
2022-04-16 17:29:20 -0700

    make jQuery use explicit

--- webpack.config.js
+++ webpack.config.js
@@ -93,15 +93,6 @@ module.exports = (env, argv) => {
       new webpack.DefinePlugin({
         "process.env.NODE_ENV": isDevelopment ? '"development"' : '"production"',
       }),
-      // http://stackoverflow.com/questions/29080148/expose-jquery-to-real-window-object-with-webpack
-      new webpack.ProvidePlugin({
-        // Automtically detect jQuery and $ as free var in modules
-        // and inject the jquery library
-        // This is required by many jquery plugins
-        jquery: "jquery",
-        jQuery: "jquery",
-        $: "jquery",
-      }),
       new HtmlWebpackPlugin(htmlConfig),
       new MiniCssExtractPlugin({
         filename: "[name].css",
