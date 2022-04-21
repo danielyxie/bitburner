3af4bd48ebd96ef17be88066a1ace478b38cf492
nickofolas <nickofolas@gmail.com>
2022-04-20 20:19:28 -0500

    Migration for cleaning up obsolete properties

--- src/SaveObject.tsx
+++ src/SaveObject.tsx
@@ -398,6 +398,44 @@ function evaluateVersionCompatibility(ver: string | number): void {
     if (ver < 15) {
       (Settings as any).EditorTheme = { ...defaultMonacoTheme };
     }
+    if (ver < 16) {
+      for (const property of [
+        "hacking_chance_mult",
+        "hacking_speed_mult",
+        "hacking_money_mult",
+        "hacking_grow_mult",
+        "hacking_mult",
+        "hacking_exp_mult",
+        "strength_mult",
+        "strength_exp_mult",
+        "defense_mult",
+        "defense_exp_mult",
+        "dexterity_mult",
+        "dexterity_exp_mult",
+        "agility_mult",
+        "agility_exp_mult",
+        "charisma_mult",
+        "charisma_exp_mult",
+        "hacknet_node_money_mult",
+        "hacknet_node_purchase_cost_mult",
+        "hacknet_node_ram_cost_mult",
+        "hacknet_node_core_cost_mult",
+        "hacknet_node_level_cost_mult",
+        "company_rep_mult",
+        "faction_rep_mult",
+        "work_money_mult",
+        "crime_success_mult",
+        "crime_money_mult",
+        "bladeburner_max_stamina_mult",
+        "bladeburner_stamina_gain_mult",
+        "bladeburner_analysis_mult",
+        "bladeburner_success_chance_mult",
+      ]) {
+        if (anyPlayer.hasOwnProperty(property)) {
+          delete anyPlayer[property];
+        }
+      }
+    }
   }
 }
 
