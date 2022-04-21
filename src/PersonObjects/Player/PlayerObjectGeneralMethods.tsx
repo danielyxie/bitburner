a914d8653a317953536bf2392c1b67c503ac5b3b
nickofolas <nickofolas@gmail.com>
2022-04-20 20:51:38 -0500

    Update `Player.resetMultipliers`

--- src/PersonObjects/Player/PlayerObjectGeneralMethods.tsx
+++ src/PersonObjects/Player/PlayerObjectGeneralMethods.tsx
@@ -279,43 +279,48 @@ export function updateSkillLevels(this: IPlayer): void {
 }
 
 export function resetMultipliers(this: IPlayer): void {
-  this.mults.hacking_chance = 1;
-  this.mults.hacking_speed = 1;
-  this.mults.hacking_money = 1;
-  this.mults.hacking_grow = 1;
+  Object.assign(this.mults, {
+    hacking_chance: 1,
+    hacking_speed: 1,
+    hacking_money: 1,
+    hacking_grow: 1,
 
-  this.mults.hacking = 1;
-  this.mults.strength = 1;
-  this.mults.defense = 1;
-  this.mults.dexterity = 1;
-  this.mults.agility = 1;
-  this.mults.charisma = 1;
+    hacking: 1,
+    hacking_exp: 1,
 
-  this.mults.hacking_exp = 1;
-  this.mults.strength_exp = 1;
-  this.mults.defense_exp = 1;
-  this.mults.dexterity_exp = 1;
-  this.mults.agility_exp = 1;
-  this.mults.charisma_exp = 1;
+    strength: 1,
+    strength_exp: 1,
 
-  this.mults.company_rep = 1;
-  this.mults.faction_rep = 1;
+    defense: 1,
+    defense_exp: 1,
 
-  this.mults.crime_money = 1;
-  this.mults.crime_success = 1;
+    dexterity: 1,
+    dexterity_exp: 1,
 
-  this.mults.hacknet_node_money = 1;
-  this.mults.hacknet_node_purchase_cost = 1;
-  this.mults.hacknet_node_ram_cost = 1;
-  this.mults.hacknet_node_core_cost = 1;
-  this.mults.hacknet_node_level_cost = 1;
+    agility: 1,
+    agility_exp: 1,
 
-  this.mults.work_money = 1;
+    charisma: 1,
+    charisma_exp: 1,
 
-  this.mults.bladeburner_max_stamina = 1;
-  this.mults.bladeburner_stamina_gain = 1;
-  this.mults.bladeburner_analysis = 1;
-  this.mults.bladeburner_success_chance = 1;
+    hacknet_node_money: 1,
+    hacknet_node_purchase_cost: 1,
+    hacknet_node_ram_cost: 1,
+    hacknet_node_core_cost: 1,
+    hacknet_node_level_cost: 1,
+
+    company_rep: 1,
+    faction_rep: 1,
+    work_money: 1,
+
+    crime_success: 1,
+    crime_money: 1,
+
+    bladeburner_max_stamina: 1,
+    bladeburner_stamina_gain: 1,
+    bladeburner_analysis: 1,
+    bladeburner_success_chance: 1,
+  });
 }
 
 export function hasProgram(this: IPlayer, programName: string): boolean {
