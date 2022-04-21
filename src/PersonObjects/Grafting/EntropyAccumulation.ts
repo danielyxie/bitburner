5c78ff0bec1d5b7f62a460dd4c05ed8ab903f06e
nickofolas <nickofolas@gmail.com>
2022-04-20 20:59:14 -0500

    Use `Object.assign` to copy mults for Entropy

--- src/PersonObjects/Grafting/EntropyAccumulation.ts
+++ src/PersonObjects/Grafting/EntropyAccumulation.ts
@@ -5,7 +5,7 @@ import { IPlayer } from "../IPlayer";
 
 export const calculateEntropy = (player: IPlayer, stacks = 1): IMap<number> => {
   // Copy mults object
-  const multipliers: IMap<number> = JSON.parse(JSON.stringify(player.mults));
+  const multipliers: IMap<number> = Object.assign({}, player.mults);
 
   for (const [mult, val] of Object.entries(multipliers)) {
     multipliers[mult] = val * CONSTANTS.EntropyEffect ** stacks;
