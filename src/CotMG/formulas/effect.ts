c2b4a5b52a2162d2e49c7317b0a60a349984eb47
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-05-19 02:48:50 -0400

    Update codebase for stanek

--- src/CotMG/formulas/effect.ts
+++ src/CotMG/formulas/effect.ts
@@ -1,9 +1,10 @@
+import { shuffle } from "lodash";
 import { BitNodeMultipliers } from "../../BitNode/BitNodeMultipliers";
 
-export function CalculateEffect(avgCharge: number, numCharge: number, power: number, boost: number): number {
+export function CalculateEffect(highestCharge: number, numCharge: number, power: number, boost: number): number {
   return (
     1 +
-    (Math.log(avgCharge + 1) / (Math.log(1.8) * 100)) *
+    (Math.log(highestCharge + 1) / 60) *
       Math.pow((numCharge + 1) / 5, 0.07) *
       power *
       boost *
