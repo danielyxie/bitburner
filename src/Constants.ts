26eff1249b0d52a6eed3644a3e0ec0ac64b62e33
nickofolas <nickofolas@gmail.com>
2022-05-19 01:22:33 -0500

    Bump save version

--- src/Constants.ts
+++ src/Constants.ts
@@ -89,7 +89,7 @@ export const CONSTANTS: {
   LatestUpdate: string;
 } = {
   VersionString: "1.6.4",
-  VersionNumber: 18,
+  VersionNumber: 19,
 
   // Speed (in ms) at which the main loop is updated
   _idleSpeed: 200,
