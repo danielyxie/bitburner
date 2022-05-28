2a5b8de2fac3f2dfec4d9e174dac899631b94d99
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-05-27 20:28:19 -0400

    comment some code out for now

--- src/PersonObjects/Work/CrimeWork.ts
+++ src/PersonObjects/Work/CrimeWork.ts
@@ -36,112 +36,112 @@ export class CrimeWork extends Work {
     if (cancelled) return "";
     const crime = this.getCrime();
     const success = determineCrimeSuccess(p, this.crimeType);
-    let hackExp = crime.hacking_exp;
-    let strExp = crime.strength_exp;
-    let defExp = crime.defense_exp;
-    let dexExp = crime.dexterity_exp;
-    let agiExp = crime.agility_exp;
-    let chaExp = crime.charisma_exp;
-    let intExp = crime.intelligence_exp;
-    let money = crime.money * p.crime_money_mult * BitNodeMultipliers.CrimeMoney;
+    const hackExp = crime.hacking_exp;
+    const strExp = crime.strength_exp;
+    const defExp = crime.defense_exp;
+    const dexExp = crime.dexterity_exp;
+    const agiExp = crime.agility_exp;
+    const chaExp = crime.charisma_exp;
+    const intExp = crime.intelligence_exp;
+    const money = crime.money * p.crime_money_mult * BitNodeMultipliers.CrimeMoney;
 
-    if (success) {
-      p.karma -= crime.karma;
-      p.numPeopleKilled += crime.kills;
-      const ws = this.singFnCrimeWorkerScript;
-      if (this.committingCrimeThruSingFn && ws !== null) {
-        if (ws.disableLogs.ALL == null && ws.disableLogs.commitCrime == null) {
-          ws.scriptRef.log(
-            "SUCCESS: Crime successful! Gained " +
-              numeralWrapper.formatMoney(money) +
-              ", " +
-              numeralWrapper.formatExp(hackExp) +
-              " hack exp, " +
-              numeralWrapper.formatExp(strExp) +
-              " str exp, " +
-              numeralWrapper.formatExp(defExp) +
-              " def exp, " +
-              numeralWrapper.formatExp(dexExp) +
-              " dex exp, " +
-              numeralWrapper.formatExp(agiExp) +
-              " agi exp, " +
-              numeralWrapper.formatExp(chaExp) +
-              " cha exp.",
-          );
-        }
-      } else {
-        dialogBoxCreate(
-          <>
-            Crime successful!
-            <br />
-            <br />
-            You gained:
-            <br />
-            <Money money={money} />
-            <br />
-            {numeralWrapper.formatExp(hackExp)} hacking experience <br />
-            {numeralWrapper.formatExp(strExp)} strength experience
-            <br />
-            {numeralWrapper.formatExp(defExp)} defense experience
-            <br />
-            {numeralWrapper.formatExp(dexExp)} dexterity experience
-            <br />
-            {numeralWrapper.formatExp(agiExp)} agility experience
-            <br />
-            {numeralWrapper.formatExp(chaExp)} charisma experience
-          </>,
-        );
-      }
-    } else {
-      hackExp /= 4;
-      strExp /= 4;
-      defExp /= 4;
-      dexExp /= 4;
-      agiExp /= 4;
-      chaExp /= 4;
-      intExp = 0;
-      money = 0;
-      const ws = this.singFnCrimeWorkerScript;
-      if (this.committingCrimeThruSingFn && ws !== null) {
-        if (ws.disableLogs.ALL == null && ws.disableLogs.commitCrime == null) {
-          ws.scriptRef.log(
-            "FAIL: Crime failed! Gained " +
-              numeralWrapper.formatExp(hackExp) +
-              " hack exp, " +
-              numeralWrapper.formatExp(strExp) +
-              " str exp, " +
-              numeralWrapper.formatExp(defExp) +
-              " def exp, " +
-              numeralWrapper.formatExp(dexExp) +
-              " dex exp, " +
-              numeralWrapper.formatExp(agiExp) +
-              " agi exp, " +
-              numeralWrapper.formatExp(chaExp) +
-              " cha exp.",
-          );
-        }
-      } else {
-        dialogBoxCreate(
-          <>
-            Crime failed!
-            <br />
-            <br />
-            You gained:
-            <br />
-            {numeralWrapper.formatExp(hackExp)} hacking experience <br />
-            {numeralWrapper.formatExp(strExp)} strength experience
-            <br />
-            {numeralWrapper.formatExp(defExp)} defense experience
-            <br />
-            {numeralWrapper.formatExp(dexExp)} dexterity experience
-            <br />
-            {numeralWrapper.formatExp(agiExp)} agility experience
-            <br />
-            {numeralWrapper.formatExp(chaExp)} charisma experience
-          </>,
-        );
-      }
-    }
+    // if (success) {
+    //   p.karma -= crime.karma;
+    //   p.numPeopleKilled += crime.kills;
+    //   const ws = this.singFnCrimeWorkerScript;
+    //   if (this.committingCrimeThruSingFn && ws !== null) {
+    //     if (ws.disableLogs.ALL == null && ws.disableLogs.commitCrime == null) {
+    //       ws.scriptRef.log(
+    //         "SUCCESS: Crime successful! Gained " +
+    //           numeralWrapper.formatMoney(money) +
+    //           ", " +
+    //           numeralWrapper.formatExp(hackExp) +
+    //           " hack exp, " +
+    //           numeralWrapper.formatExp(strExp) +
+    //           " str exp, " +
+    //           numeralWrapper.formatExp(defExp) +
+    //           " def exp, " +
+    //           numeralWrapper.formatExp(dexExp) +
+    //           " dex exp, " +
+    //           numeralWrapper.formatExp(agiExp) +
+    //           " agi exp, " +
+    //           numeralWrapper.formatExp(chaExp) +
+    //           " cha exp.",
+    //       );
+    //     }
+    //   } else {
+    //     dialogBoxCreate(
+    //       <>
+    //         Crime successful!
+    //         <br />
+    //         <br />
+    //         You gained:
+    //         <br />
+    //         <Money money={money} />
+    //         <br />
+    //         {numeralWrapper.formatExp(hackExp)} hacking experience <br />
+    //         {numeralWrapper.formatExp(strExp)} strength experience
+    //         <br />
+    //         {numeralWrapper.formatExp(defExp)} defense experience
+    //         <br />
+    //         {numeralWrapper.formatExp(dexExp)} dexterity experience
+    //         <br />
+    //         {numeralWrapper.formatExp(agiExp)} agility experience
+    //         <br />
+    //         {numeralWrapper.formatExp(chaExp)} charisma experience
+    //       </>,
+    //     );
+    //   }
+    // } else {
+    //   hackExp /= 4;
+    //   strExp /= 4;
+    //   defExp /= 4;
+    //   dexExp /= 4;
+    //   agiExp /= 4;
+    //   chaExp /= 4;
+    //   intExp = 0;
+    //   money = 0;
+    //   const ws = this.singFnCrimeWorkerScript;
+    //   if (this.committingCrimeThruSingFn && ws !== null) {
+    //     if (ws.disableLogs.ALL == null && ws.disableLogs.commitCrime == null) {
+    //       ws.scriptRef.log(
+    //         "FAIL: Crime failed! Gained " +
+    //           numeralWrapper.formatExp(hackExp) +
+    //           " hack exp, " +
+    //           numeralWrapper.formatExp(strExp) +
+    //           " str exp, " +
+    //           numeralWrapper.formatExp(defExp) +
+    //           " def exp, " +
+    //           numeralWrapper.formatExp(dexExp) +
+    //           " dex exp, " +
+    //           numeralWrapper.formatExp(agiExp) +
+    //           " agi exp, " +
+    //           numeralWrapper.formatExp(chaExp) +
+    //           " cha exp.",
+    //       );
+    //     }
+    //   } else {
+    //     dialogBoxCreate(
+    //       <>
+    //         Crime failed!
+    //         <br />
+    //         <br />
+    //         You gained:
+    //         <br />
+    //         {numeralWrapper.formatExp(hackExp)} hacking experience <br />
+    //         {numeralWrapper.formatExp(strExp)} strength experience
+    //         <br />
+    //         {numeralWrapper.formatExp(defExp)} defense experience
+    //         <br />
+    //         {numeralWrapper.formatExp(dexExp)} dexterity experience
+    //         <br />
+    //         {numeralWrapper.formatExp(agiExp)} agility experience
+    //         <br />
+    //         {numeralWrapper.formatExp(chaExp)} charisma experience
+    //       </>,
+    //     );
+    //   }
+    // }
 
     p.gainHackingExp(hackExp);
     p.gainStrengthExp(strExp);
