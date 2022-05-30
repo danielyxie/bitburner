bad8421d25ecbb090022f46b55ccc81c07af47c9
lordyoyo <lordyoyo@gmail.com>
2022-05-31 00:05:04 +0300

    nerfed infiltration rewards to take into account BN12 multipliers

--- src/Infiltration/formulas/game.ts
+++ src/Infiltration/formulas/game.ts
@@ -1,4 +1,5 @@
 import { IPlayer } from "../../PersonObjects/IPlayer";
+import { BitNodeMultipliers } from "../../BitNode/BitNodeMultipliers";
 import { calculateSkill } from "../../PersonObjects/formulas/skill";
 
 function calculateRawDiff(player: IPlayer, stats: number, startingDifficulty: number): number {
@@ -16,10 +17,10 @@ export function calculateDifficulty(player: IPlayer, startingSecurityLevel: numb
 export function calculateReward(player: IPlayer, startingSecurityLevel: number): number {
   const xpMult = 10 * 60 * 15;
   const total =
-    calculateSkill(player.strength_exp_mult * xpMult, player.strength_mult) +
-    calculateSkill(player.defense_exp_mult * xpMult, player.defense_mult) +
-    calculateSkill(player.agility_exp_mult * xpMult, player.agility_mult) +
-    calculateSkill(player.dexterity_exp_mult * xpMult, player.dexterity_mult) +
-    calculateSkill(player.charisma_exp_mult * xpMult, player.charisma_mult);
+    calculateSkill(player.strength_exp_mult * xpMult, player.strength_mult * BitNodeMultipliers.StrengthLevelMultiplier) +
+    calculateSkill(player.defense_exp_mult * xpMult, player.defense_mult * BitNodeMultipliers.DefenseLevelMultiplier) +
+    calculateSkill(player.dexterity_exp_mult * xpMult, player.dexterity_mult * BitNodeMultipliers.DexterityLevelMultiplier) +
+    calculateSkill(player.agility_exp_mult * xpMult, player.agility_mult * BitNodeMultipliers.AgilityLevelMultiplier) +
+    calculateSkill(player.charisma_exp_mult * xpMult, player.charisma_mult * BitNodeMultipliers.CharismaLevelMultiplier);
   return calculateRawDiff(player, total, startingSecurityLevel);
 }
