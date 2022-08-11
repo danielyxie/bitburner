765cfd0c9a9075b48c7045d3691236ef8f3737f8
Zoë Hoekstra <altlockg@gmail.com>
2022-08-11 19:44:55 +0200

    Clear interval when functional connectionBauble gets unmounted

--- src/GameOptions/ui/ConnectionBauble.tsx
+++ src/GameOptions/ui/ConnectionBauble.tsx
@@ -8,9 +8,10 @@ export const ConnectionBauble = (props: baubleProps): React.ReactElement => {
   const [connection, setConnection] = useState(props.callback());
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       setConnection(props.callback());
     }, 1000);
+    return () => clearInterval(timer);
   });
 
   return <div className="ConnectionBauble">{connection ? "Connected" : "Disconnected"}</div>;
