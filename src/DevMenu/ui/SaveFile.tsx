4cdc1bc51f380eb1a13d4a228aa9e0cc8b2be6cc
Conner Tennery <code@connertennery.dev>
2022-08-19 02:42:54 -0700

    Modified Dev SaveFile to operate closer to typical import implemented - restore scripts - restore augmentations - restore source files

--- src/DevMenu/ui/SaveFile.tsx
+++ src/DevMenu/ui/SaveFile.tsx
@@ -6,28 +6,22 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import Typography from "@mui/material/Typography";
-import { saveObject } from "../../SaveObject";
+import { BitburnerSaveObject, ImportData, saveObject } from "../../SaveObject";
 import { SnackbarEvents, ToastVariant } from "../../ui/React/Snackbar";
 import { Upload } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import { OptionSwitch } from "../../ui/React/OptionSwitch";
+import { IReviverValue } from "src/utils/JSONReviver";
+import { Server } from "src/Server/Server";
+import { PlayerObject } from "src/PersonObjects/Player/PlayerObject";
 
 export function SaveFile(): React.ReactElement {
   const importInput = useRef<HTMLInputElement>(null);
-  const [saveFile, setSaveFile] = useState("");
+
+  const [importData, setImportData] = useState<ImportData | null>(null);
   const [restoreScripts, setRestoreScripts] = useState(true);
   const [restoreAugs, setRestoreAugs] = useState(true);
   const [restoreSFs, setRestoreSFs] = useState(true);
-
-  async function onImport(event: React.ChangeEvent<HTMLInputElement>): Promise<void> {
-    try {
-      const base64Save = await saveObject.getImportStringFromFile(event.target.files);
-      const save = atob(base64Save);
-      setSaveFile(save);
-    } catch (e: unknown) {
-      SnackbarEvents.emit(String(e), ToastVariant.ERROR, 5000);
-    }
-  }
 
   function startImport(): void {
     if (!window.File || !window.FileReader || !window.FileList || !window.Blob) return;
@@ -36,10 +30,60 @@ export function SaveFile(): React.ReactElement {
     ii.click();
   }
 
-  function doRestore(): void {
-    const save = JSON.parse(saveFile);
-    // TODO: Continue here.
-    console.error(save);
+  async function onImport(event: React.ChangeEvent<HTMLInputElement>): Promise<void> {
+    try {
+      const base64Save = await saveObject.getImportStringFromFile(event.target.files);
+      const data = await saveObject.getImportDataFromString(base64Save);
+      setImportData(data);
+    } catch (e: unknown) {
+      SnackbarEvents.emit(String(e), ToastVariant.ERROR, 5000);
+    }
+  }
+
+  async function doRestore(): Promise<void> {
+    if (!importData) return;
+
+    try {
+      if (!restoreAugs || !restoreSFs || !restoreScripts) {
+        //Remove unwanted data
+        const parsedSave = JSON.parse(window.atob(importData.base64));
+        const parsedRoot = BitburnerSaveObject.fromJSON(parsedSave);
+
+        if (!restoreAugs || !restoreSFs) {
+          const parsedPlayer = JSON.parse(parsedRoot.PlayerSave) as IReviverValue<PlayerObject>;
+          if (!restoreAugs) {
+            parsedPlayer.data.augmentations = [];
+            if (importData.playerData) {
+              importData.playerData.augmentations = 0;
+            }
+          }
+
+          if (!restoreSFs) {
+            parsedPlayer.data.sourceFiles = [];
+            if (importData.playerData) {
+              importData.playerData.sourceFiles = 0;
+            }
+          }
+
+          parsedRoot.PlayerSave = JSON.stringify(parsedPlayer);
+        }
+
+        if (!restoreScripts) {
+          const parsedServers = JSON.parse(parsedRoot.AllServersSave)
+          for (const server of Object.values(parsedServers) as IReviverValue<Server>[]) {
+            server.data.scripts = []
+          }
+
+          parsedRoot.AllServersSave = JSON.stringify(parsedServers);
+        }
+
+        importData.base64 = window.btoa(JSON.stringify(parsedRoot));
+      }
+
+      await saveObject.importGame(importData.base64);
+    } catch (e: unknown) {
+      SnackbarEvents.emit(String(e), ToastVariant.ERROR, 5000);
+    }
   }
 
   return (
@@ -53,7 +97,7 @@ export function SaveFile(): React.ReactElement {
           <input ref={importInput} type="file" hidden onChange={onImport} />
         </Button>
         <br />
-        {saveFile !== "" && (
+        {importData !== null && (
           <>
             <OptionSwitch
               checked={restoreScripts}
