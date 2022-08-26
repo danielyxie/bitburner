197081dd3ea2e6a3afd140cd3070262c7f657d5e
MPJ / Quadryo <104721042+MPJ-K@users.noreply.github.com>
2022-08-26 20:59:26 +0200

    Fix #3963
    Adds a previously missing `throw` to allow `bladeburner.setActionLevel()` to properly produce errors and prevent invalid action levels from being set.

--- src/NetscriptFunctions/Bladeburner.ts
+++ src/NetscriptFunctions/Bladeburner.ts
@@ -250,7 +250,7 @@ export function NetscriptBladeburner(): InternalAPI<INetscriptBladeburner> {
         checkBladeburnerAccess(ctx);
         const action = getBladeburnerActionObject(ctx, type, name);
         if (level < 1 || level > action.maxLevel) {
-          helpers.makeRuntimeErrorMsg(ctx, `Level must be between 1 and ${action.maxLevel}, is ${level}`);
+          throw helpers.makeRuntimeErrorMsg(ctx, `Level must be between 1 and ${action.maxLevel}, is ${level}`);
         }
         action.level = level;
       },
