0e67fbee21e4862ce57b3262c3a260a5015e9248
Snarling <84951833+Snarling@users.noreply.github.com>
2022-08-29 02:41:17 -0400

    Unify error handling

--- src/Netscript/killWorkerScript.ts
+++ src/Netscript/killWorkerScript.ts
@@ -62,7 +62,8 @@ function stopAndCleanUpWorkerScript(ws: WorkerScript): void {
   //Clean up any ongoing netscriptDelay
   if (ws.delay) clearTimeout(ws.delay);
   ws.delayReject?.(new ScriptDeath(ws));
-
+  ws.env.runningFn = "";
+  
   if (typeof ws.atExit === "function") {
     try {
       ws.env.stopFlag = false;
