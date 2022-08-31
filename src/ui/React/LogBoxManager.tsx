2592c6ccd89d5559c9cc3cdf99416eb1c57edca2
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-08-31 12:26:02 -0400

    allbuild commit 87054f61

--- src/ui/React/LogBoxManager.tsx
+++ src/ui/React/LogBoxManager.tsx
@@ -171,7 +171,6 @@ function LogWindow(props: IProps): React.ReactElement {
     const node = rootRef?.current;
     if (!node) return;
     const state = node.state as { x: number; y: number };
-    console.log(`before ${state.x} ${state.y}`);
     state.x = x;
     state.y = y;
   };
@@ -315,8 +314,8 @@ function LogWindow(props: IProps): React.ReactElement {
         ref={container}
       >
         <ResizableBox
-          height={size[0]}
-          width={size[1]}
+          width={size[0]}
+          height={size[1]}
           onResize={onResize}
           minConstraints={minConstraints}
           handle={
