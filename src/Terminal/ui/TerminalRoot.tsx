22a35af6b683e94fa450241179689e4352f31daa
G4mingJon4s <Jthelker@gmail.com>
2022-09-14 15:59:55 +0200

    Added hover style change for better recognition

--- src/Terminal/ui/TerminalRoot.tsx
+++ src/Terminal/ui/TerminalRoot.tsx
@@ -125,7 +125,7 @@ export function TerminalRoot({ terminal, router, player }: IProps): React.ReactE
                     paragraph={false}
                     onClick={() => terminal.connectToServer(player, item.hostname)}
                   >
-                    <Typography sx={{ textDecoration: 'underline' }}>{item.hostname}</Typography>
+                    <Typography sx={{ textDecoration: 'underline', "&:hover": { textDecoration: 'none'} }}>{item.hostname}</Typography>
                   </MuiLink>
                 </>
               )}
