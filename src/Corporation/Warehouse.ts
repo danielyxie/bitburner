fb69046d3555bcd7fd3ab58252985e5a3c4f7ff2
Shiiyu <faithiikittii@gmail.com>
2022-09-23 17:43:41 -0400

    fix: :pencil2: Initiatizes -> Initializes

--- src/Corporation/Warehouse.ts
+++ src/Corporation/Warehouse.ts
@@ -109,7 +109,7 @@ export class Warehouse {
     return Generic_toJSON("Warehouse", this);
   }
 
-  // Initiatizes a Warehouse object from a JSON save state.
+  // Initializes a Warehouse object from a JSON save state.
   static fromJSON(value: IReviverValue): Warehouse {
     return Generic_fromJSON(Warehouse, value.data);
   }
