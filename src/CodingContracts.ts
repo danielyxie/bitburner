d51063e505bba2d575ca3abe698583f4d46e52af
Shiiyu <faithiikittii@gmail.com>
2022-09-23 22:03:43 -0400

    fix: :pencil2: Initiatizes -> Initializes

--- src/CodingContracts.ts
+++ src/CodingContracts.ts
@@ -191,7 +191,7 @@ export class CodingContract {
   }
 
   /**
-   * Initiatizes a CodingContract from a JSON save state.
+   * Initializes a CodingContract from a JSON save state.
    */
   static fromJSON(value: IReviverValue): CodingContract {
     return Generic_fromJSON(CodingContract, value.data);
