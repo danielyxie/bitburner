4b24db9ce210a20829143ded82d7db3ad13fea93
Shiiyu <faithiikittii@gmail.com>
2022-09-25 17:15:31 -0400

    fix: :pencil2: whenver -> whenever

--- src/StockMarket/StockMarketConstants.ts
+++ src/StockMarket/StockMarketConstants.ts
@@ -1,5 +1,5 @@
 /**
  * How many stock market 'ticks' before a 'cycle' is triggered.
- * A 'tick' is whenver stock prices update
+ * A 'tick' is whenever stock prices update
  */
 export const TicksPerCycle = 75;
