f4252d2a956811d1dfd902c33e2844d53a7dbcb0
Shiiyu <faithiikittii@gmail.com>
2022-09-25 17:02:55 -0400

    fix: :pencil2: suggested changes 4 + #4150

--- src/CotMG/StaneksGift.ts
+++ src/CotMG/StaneksGift.ts
@@ -57,20 +57,20 @@ export class StaneksGift extends BaseGift {
   }
 
   effect(fragment: ActiveFragment): number {
-    // Find all the neighbooring cells
-    const cells = fragment.neighboors();
-    // find the neighbooring active fragments.
+    // Find all the neighboring cells
+    const cells = fragment.neighbors();
+    // find the neighboring active fragments.
     const maybeFragments = cells.map((n) => this.fragmentAt(n[0], n[1]));
 
     // Filter out undefined with typescript "Type guard". Whatever
-    let neighboors = maybeFragments.filter((v: ActiveFragment | undefined): v is ActiveFragment => !!v);
+    let neighbors = maybeFragments.filter((v: ActiveFragment | undefined): v is ActiveFragment => !!v);
 
-    neighboors = neighboors.filter((fragment) => fragment.fragment().type === FragmentType.Booster);
+    neighbors = neighbors.filter((fragment) => fragment.fragment().type === FragmentType.Booster);
     let boost = 1;
 
-    neighboors = neighboors.filter((v, i, s) => s.indexOf(v) === i);
-    for (const neighboor of neighboors) {
-      boost *= neighboor.fragment().power;
+    neighbors = neighbors.filter((v, i, s) => s.indexOf(v) === i);
+    for (const neighbor of neighbors) {
+      boost *= neighbor.fragment().power;
     }
     return CalculateEffect(fragment.highestCharge, fragment.numCharge, fragment.fragment().power, boost);
   }
