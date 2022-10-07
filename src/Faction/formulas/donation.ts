ea39e5e1f46ddc962a7463d09b646d80ca83a059
Fabian Dellwing <fabian.dellwing@mbconnectline.de>
2022-10-07 21:28:19 +0200

    Include BN multiplier in faction donation

--- src/Faction/formulas/donation.ts
+++ src/Faction/formulas/donation.ts
@@ -1,6 +1,7 @@
 import { CONSTANTS } from "../../Constants";
+import { BitNodeMultipliers } from "../../BitNode/BitNodeMultipliers";
 import { Person } from "../../PersonObjects/Person";
 
 export function repFromDonation(amt: number, person: Person): number {
-  return (amt / CONSTANTS.DonateMoneyToRepDivisor) * person.mults.faction_rep;
+  return (amt / CONSTANTS.DonateMoneyToRepDivisor) * person.mults.faction_rep * BitNodeMultipliers.FactionWorkRepGain;
 }
