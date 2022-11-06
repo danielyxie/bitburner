c17af3ad3558ba92e811da6bb235cefafb0f4779
Rai <58925163+rai68@users.noreply.github.com>
2022-11-07 08:30:01 +1000

    UI: SoftResetButton.tsx Tooltip changed to make more sense (#186)

--- src/ui/React/SoftResetButton.tsx
+++ src/ui/React/SoftResetButton.tsx
@@ -28,7 +28,7 @@ export function SoftResetButton({
 
   return (
     <>
-      <Tooltip title="Perform a soft reset. Resets everything as if you had just purchased an Augmentation.">
+      <Tooltip title="Perform a soft reset. Resets everything as if you had just installed Augmentations without installing them.">
         <Button startIcon={<RestartAltIcon />} color={color} onClick={handleButtonClick}>
           Soft Reset
         </Button>
